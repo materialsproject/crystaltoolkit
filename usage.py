fcce8992b17117091ffb11dc33160d572d53685c
Matthew Horton <mkhorton@users.noreply.github.com>
2018-04-27 10:32:43 -0700

    Add structure div (can be hidden later)

--- usage.py
+++ usage.py
@@ -3,17 +3,20 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 
+import json
+
 from dash.dependencies import Input, Output, State
+
+from monty.serialization import loadfn
+
+from pymatgen import MPRester, Structure
+from pymatgen.vis.structure_vis_mp import MaterialsProjectStructureVis
+from pymatgen.analysis.local_env import NearNeighbors
 
 app = dash.Dash('')
 
 app.scripts.config.serve_locally = True
 app.css.append_css({'external_url': 'https://codepen.io/chriddyp/pen/bWLwgP.css'})
-
-from monty.serialization import loadfn
-from pymatgen import MPRester
-from pymatgen.vis.structure_vis_mp import MaterialsProjectStructureVis
-from pymatgen.analysis.local_env import NearNeighbors
 
 mpr = MPRester()
 
@@ -21,13 +24,19 @@ DEFAULT_STRUCTURE = loadfn('default_structure.json')
 DEFAULT_COLOR_SCHEME = 'VESTA'
 DEFAULT_BONDING_METHOD = 'MinimumOKeeffeNN'
 
-formula_input_button = html.Div([
+AVAILABLE_BONDING_METHODS = [str(c.__name__)
+                             for c in NearNeighbors.__subclasses__()]
+
+# to help with readability, each component of the app is defined
+# in a modular way below; these are all then included in the app.layout
+
+LAYOUT_FORMULA_INPUT = html.Div([
     dcc.Input(id='input-box', type='text', placeholder='Enter a formula or mp-id'),
     html.Span(' '),
     html.Button('Load', id='button')
 ])
 
-draw_options = dcc.Checklist(
+LAYOUT_VISIBILITY_OPTIONS = dcc.Checklist(
     id='draw_options',
     options=[
         {'label': 'Draw Atoms', 'value': 'atoms'},
@@ -38,23 +47,30 @@ draw_options = dcc.Checklist(
     values=['atoms', 'bonds', 'polyhedra', 'unit_cell']
 )
 
-bonding_methods = [str(c.__name__) for c in NearNeighbors.__subclasses__()]
-bonding_options = html.Div([html.Label("Bonding Algorithm"), dcc.Dropdown(
+LAYOUT_BONDING_DROPDOWN = html.Div([html.Label("Bonding Algorithm"), dcc.Dropdown(
     id='bonding_options',
     options=[
-        {'label': method, 'value': method} for method in bonding_methods
+        {'label': method, 'value': method} for method in AVAILABLE_BONDING_METHODS
     ],
     value=DEFAULT_BONDING_METHOD
 )])
 
-color_options = html.Div([html.Label("Color Code"), dcc.Dropdown(
-    id='color_options',
+LAYOUT_COLOR_SCHEME_DROPDOWN = html.Div([html.Label("Color Code"), dcc.Dropdown(
+    id='color_schemes',
     options=[
         {'label': option, 'value': option} for option in ['VESTA', 'Jmol']
     ],
     value=DEFAULT_COLOR_SCHEME
 )])
 
+LAYOUT_DEVELOPER_TEXTBOX = dcc.Textarea(
+    id='structure',
+    placeholder='Developer console',
+    value='',
+    style={'width': '100%', 'overflow-y': 'scroll', 'height': '400px'}
+)
+
+# master app layout, includes layouts defined above
 app.layout = html.Div([
     html.Br(),
     html.H1('MP Viewer', style={'text-align': 'center'}),
@@ -83,33 +99,24 @@ app.layout = html.Div([
                 className='three columns',
                 style={'text-align': 'left'},
                 children=[
-                    formula_input_button,
+                    LAYOUT_FORMULA_INPUT,
                     html.Br(),
-                    draw_options,
+                    LAYOUT_VISIBILITY_OPTIONS,
                     html.Br(),
-                    bonding_options,
+                    LAYOUT_BONDING_DROPDOWN,
                     html.Br(),
-                    color_options
+                    LAYOUT_COLOR_SCHEME_DROPDOWN,
+                    html.Br(),
+                    LAYOUT_DEVELOPER_TEXTBOX
                 ]
             ),
             html.Div(className='one columns')
-        ]),
-    html.Div(
-        className='row',
-        children=[
-            dcc.Textarea(
-                id='structure',
-                placeholder='Developer console',
-                value='',
-                style={'width': '100%'}
-            )
-        ]
-    )
+        ])
 ])
 
 
 @app.callback(
-    Output('color_options', 'options'),
+    Output('color_schemes', 'options'),
     [Input('button', 'n_clicks')],
     [State('input-box', 'value')])
 def update_color_options(n_clicks, input_formula_mpid):
@@ -145,23 +152,32 @@ def update_color_options(n_clicks, input_formula_mpid):
         {'label': pretty_rewrite(option), 'value': option} for option in options
     ]
 
-
 @app.callback(
-    Output('viewer', 'data'),
-    [Input('button', 'n_clicks'),
-     Input('bonding_options', 'value'),
-     Input('color_options', 'value')],
+    Output('structure', 'value'),
+    [Input('button', 'n_clicks')],
     [State('input-box', 'value')])
-def update_crystal_displayed(n_clicks, bonding_option, color_option, input_formula_mpid):
+def retrieve_structure(n_clicks, input_formula_mpid):
 
     if not input_formula_mpid:
         structure = DEFAULT_STRUCTURE
     else:
         structure = mpr.get_structures(input_formula_mpid)[0]
 
+    return json.dumps(json.loads(structure.to_json()), indent=4)
+
+
+@app.callback(
+    Output('viewer', 'data'),
+    [Input('structure', 'value'),
+     Input('bonding_options', 'value'),
+     Input('color_schemes', 'value')])
+def update_crystal_displayed(structure, bonding_option, color_scheme):
+
+    structure = Structure.from_str(structure, fmt='json')
+
     crystal_json = MaterialsProjectStructureVis(structure,
                                                 bonding_strategy=bonding_option,
-                                                color_scheme=color_option).json
+                                                color_scheme=color_scheme).json
 
     return crystal_json
 
