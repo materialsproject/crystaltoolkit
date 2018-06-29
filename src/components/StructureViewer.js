ff0397b5ef669536285d78319415fc13bcda9554
Matthew Horton <mkhorton@users.noreply.github.com>
2018-06-29 16:56:44 -0700

    Separate StructureViewer from React component

---
+++ src/components/StructureViewer.js
@@ -0,0 +1,391 @@
+import * as THREE from 'three';
+import OrbitControls from 'orbit-controls-es6';
+
+export default class StructureViewer {
+
+    static getMaterial(color) {
+		return new THREE.MeshPhongMaterial({
+			color: color,
+			shininess: 1,
+			reflectivity: 1,
+			specular: 0xffffff
+		});
+	}
+	
+	static makeCrystal(crystal_json) {
+
+		if (typeof crystal_json !== 'undefined') {
+
+
+			const crystal = new THREE.Object3D();
+			crystal.name = 'crystal';
+			crystal.castsShadow = true;
+			crystal.receivesShadow = true;
+
+			const atoms = StructureViewer.makeAtoms(crystal_json)
+			crystal.add(atoms)
+
+			const bonds = StructureViewer.makeBonds(crystal_json, atoms)
+			crystal.add(bonds)
+
+			const unit_cell = StructureViewer.makeUnitCell(crystal_json)
+			crystal.add(unit_cell)
+
+			const polyhedra = StructureViewer.makePolyhedra(crystal_json, atoms)
+			crystal.add(polyhedra)
+
+			return crystal
+
+		}
+
+	}
+
+	static makeAtoms(crystal_json) {
+
+		const scale = 0.5;
+
+		const atoms = new THREE.Object3D();
+		atoms.name = 'atoms';
+
+		crystal_json.atoms.forEach(function(atom) {
+			const radius = atom.fragments[0].radius * scale;
+			const geometry = new THREE.SphereGeometry(radius, 32, 32);
+			const color = new THREE.Color(...atom.fragments[0].color);
+			const material = StructureViewer.getMaterial(color);
+			const sphere = new THREE.Mesh(geometry, material);
+			sphere.position.set(...atom.position)
+			atoms.add(sphere);
+		});
+
+		return atoms;
+	}
+
+	static makeBonds(crystal_json, atoms) {
+
+		const bonds = new THREE.Object3D();
+		bonds.name = 'bonds';
+
+		crystal_json.bonds.forEach(function(bond) {
+
+			const sourceObj = atoms.children[bond.from_atom_index]
+			const destinationObj = atoms.children[bond.to_atom_index]
+
+			var midPoint = destinationObj.position.clone().sub(sourceObj.position);
+			const len = midPoint.length();
+			midPoint = midPoint.multiplyScalar(0.5);
+			midPoint = sourceObj.position.clone().add(midPoint)
+
+			const geometry = new THREE.CylinderGeometry(0.1, 0.1, len / 2, 16, 1);
+
+			geometry.applyMatrix(new THREE.Matrix4().makeTranslation(0, len / 4, 0));
+			geometry.applyMatrix(new THREE.Matrix4().makeRotationX(Math.PI / 2));
+
+			const material_from = StructureViewer.getMaterial(atoms.children[bond.from_atom_index].material.color);
+
+			const cylinder_from = new THREE.Mesh(geometry, material_from);
+
+			cylinder_from.translateX(sourceObj.position.x);
+			cylinder_from.translateY(sourceObj.position.y);
+			cylinder_from.translateZ(sourceObj.position.z);
+
+			cylinder_from.lookAt(midPoint);
+
+			const material_to = StructureViewer.getMaterial(atoms.children[bond.to_atom_index].material.color);
+
+			const cylinder_to = new THREE.Mesh(geometry, material_to);
+			cylinder_to.translateX(destinationObj.position.x);
+			cylinder_to.translateY(destinationObj.position.y);
+			cylinder_to.translateZ(destinationObj.position.z);
+
+			cylinder_to.lookAt(midPoint);
+
+			bonds.add(cylinder_from);
+			bonds.add(cylinder_to);
+
+		})
+
+		return bonds;
+
+	}
+
+	static makeUnitCell(crystal_json) {
+
+	    const unitcell = new THREE.Object3D();
+	    unitcell.name = 'unitcell';
+
+        for (var cell in crystal_json.unit_cell) {
+
+		    const edges = new THREE.Geometry();
+		    crystal_json.unit_cell[cell].lines.map(p => edges.vertices.push(new THREE.Vector3(...p)))
+
+		    if (crystal_json.unit_cell[cell].style != 'dashed') {
+		        const line_material = new THREE.LineDashedMaterial({
+		    		color: 0x0,
+		    		linewidth: 1,
+		    		scale: 1,
+	                dashSize: 3,
+	                gapSize: 1
+		    	})
+		    	const unitcell_object = new THREE.LineSegments(edges, line_material);
+		        unitcell.add(unitcell_object);
+		    } else {
+		        const line_material = new THREE.LineBasicMaterial({
+		    		color: 0x0,
+		    		linewidth: 1
+		    	})
+		        const unitcell_object = new THREE.LineSegments(edges, line_material);
+		        unitcell.add(unitcell_object);
+		    }
+
+        }
+
+		return unitcell
+	}
+
+	static makePolyhedra(crystal_json, atoms) {
+
+		const polyhedra = new THREE.Object3D();
+		polyhedra.name = 'polyhedra';
+		
+		this.available_polyhedra = crystal_json.polyhedra.polyhedra_types
+		this.default_polyhedra = crystal_json.polyhedra.default_polyhedra_types
+
+		for (var polyhedron_type in crystal_json.polyhedra.polyhedra_by_type) {
+
+		    const polyhedra_type_object = new THREE.Object3D();
+		    polyhedra_type_object.name = polyhedron_type;
+
+		    crystal_json.polyhedra.polyhedra_by_type[polyhedron_type].forEach(function(polyhedron) {
+
+			    const polyhedron_geometry = new THREE.Geometry();
+			    polyhedron.points.map(p => polyhedron_geometry.vertices.push(new THREE.Vector3(...p)))
+			    polyhedron.hull.map(p => polyhedron_geometry.faces.push(new THREE.Face3(...p)))
+			    polyhedron_geometry.computeFaceNormals();
+
+			    const polyhedron_color = atoms.children[polyhedron.center].material.color;
+			    const polyhedron_material = StructureViewer.getMaterial(polyhedron_color);
+                polyhedron_material.side = THREE.DoubleSide;
+
+			    const polyhedron_object = new THREE.Mesh(polyhedron_geometry, polyhedron_material)
+
+			    polyhedra_type_object.add(polyhedron_object)
+
+			})
+
+			if (!this.default_polyhedra.includes(polyhedron_type)) {
+			    polyhedra_type_object.visible = false;
+			}
+
+            polyhedra.add(polyhedra_type_object);
+
+		}
+
+		return polyhedra
+
+	}
+
+	constructor(crystal_json, dom_elt, rotationSpeed=0) {
+
+		this.start = this.start.bind(this)
+		this.stop = this.stop.bind(this)
+		this.animate = this.animate.bind(this)
+		this.rotationSpeed = rotationSpeed
+
+		// This is where all the Three.js scene construction happens
+
+		const width = dom_elt.clientWidth
+		const height = dom_elt.clientHeight
+
+		const scene = new THREE.Scene()
+		const camera = new THREE.OrthographicCamera(
+			width / -2,
+			width / 2,
+			height / 2,
+			height / -2, -2000, 2000);
+
+		camera.position.z = 2;
+
+		const renderer = new THREE.WebGLRenderer({
+			antialias: true,
+			alpha: true
+		})
+
+		renderer.setPixelRatio(window.devicePixelRatio * 1.5);
+		renderer.setClearColor(0xffffff, 0)
+		renderer.setSize(width, height)
+
+		// Lighting
+
+		const ambientLight = new THREE.AmbientLight(0xffffff, 0.002);
+		scene.add(ambientLight)
+
+		const directionalLight = new THREE.DirectionalLight(0xffffff, 0.0015);
+		directionalLight.position.set(-1, 1, 1).normalize();
+		scene.add(directionalLight);
+
+		const hemisphereLight = new THREE.HemisphereLight(0xffffff, 0x222222, 0.0015);
+		scene.add(hemisphereLight)
+
+		scene.add(camera)
+
+		if (typeof crystal_json !== 'undefined') {
+
+			const crystal = StructureViewer.makeCrystal(crystal_json);
+
+			scene.add(crystal);
+			camera.lookAt(crystal.position);
+			directionalLight.target = crystal;
+
+			const box = new THREE.Box3();
+			box.setFromObject(crystal);
+			camera.zoom = Math.min(width / (box.max.x - box.min.x),
+				height / (box.max.y - box.min.y)) * 0.6;
+			camera.updateProjectionMatrix();
+			camera.updateMatrix();
+
+			this.crystal = crystal;
+			this.available_polyhedra = crystal_json.polyhedra.polyhedra_types;
+		}
+
+		this.scene = scene
+		this.camera = camera
+		this.renderer = renderer
+
+		// Controls
+		const controls = new OrbitControls(camera, this.renderer.domElement);
+
+		dom_elt.appendChild(this.renderer.domElement)
+		this.start();
+	}
+
+	changeVisibility(visibilityOptions) {
+
+        var atoms = this.crystal.getObjectByName("atoms");
+        if (typeof atoms !== "undefined") {
+            if (visibilityOptions.atoms) {
+                atoms.visible = true;
+            } else {
+                atoms.visible = false;
+            }
+        }
+
+        var bonds = this.crystal.getObjectByName("bonds");
+        if (typeof bonds !== "undefined") {
+            if (visibilityOptions.bonds) {
+                bonds.visible = true;
+            } else {
+                bonds.visible = false;
+            }
+        }
+
+        var unitcell = this.crystal.getObjectByName("unitcell");
+        if (typeof unitcell !== "undefined") {
+            if (visibilityOptions.unitcell) {
+                unitcell.visible = true;
+            } else {
+                unitcell.visible = false;
+            }
+        }
+
+	    var polyhedra = this.crystal.getObjectByName("polyhedra");
+	    if (typeof polyhedra !== "undefined") {
+	        if (visibilityOptions.polyhedra instanceof Array) {
+	            polyhedra.visible = true
+	            for (var polyhedraType in this.available_polyhedra) {
+	                var polyhedraTypeObject = this.crystal.getObjectByName(polyhedraType);
+	                if (polyhedraType in visibilityOptions.polyhedra) {
+	                    polyhedraTypeObject.visible = true;
+	                } else {
+	                    polyhedraTypeObject.visible = false;
+	                }
+	            }
+	        } else if (visibilityOptions.polyhedra) {
+	        	polyhedra.visible = true
+	        } else {
+	        	polyhedra.visible = false
+	        }
+	    }
+
+	}
+
+
+	static disposeNode(parentObject) {
+	    // https://stackoverflow.com/questions/33152132/
+        parentObject.traverse(function (node) {
+            if (node instanceof THREE.Mesh) {
+                if (node.geometry) {
+                    node.geometry.dispose();
+                }
+                if (node.material) {
+                    var materialArray;
+                    if (node.material instanceof THREE.MeshFaceMaterial || node.material instanceof THREE.MultiMaterial) {
+                        materialArray = node.material.materials;
+                    }
+                    else if(node.material instanceof Array) {
+                        materialArray = node.material;
+                    }
+                    if(materialArray) {
+                        materialArray.forEach(function (mtrl, idx) {
+                            if (mtrl.map) mtrl.map.dispose();
+                            if (mtrl.lightMap) mtrl.lightMap.dispose();
+                            if (mtrl.bumpMap) mtrl.bumpMap.dispose();
+                            if (mtrl.normalMap) mtrl.normalMap.dispose();
+                            if (mtrl.specularMap) mtrl.specularMap.dispose();
+                            if (mtrl.envMap) mtrl.envMap.dispose();
+                            mtrl.dispose();
+                        });
+                    }
+                    else {
+                        if (node.material.map) node.material.map.dispose();
+                        if (node.material.lightMap) node.material.lightMap.dispose();
+                        if (node.material.bumpMap) node.material.bumpMap.dispose();
+                        if (node.material.normalMap) node.material.normalMap.dispose();
+                        if (node.material.specularMap) node.material.specularMap.dispose();
+                        if (node.material.envMap) node.material.envMap.dispose();
+                        node.material.dispose();
+                    }
+                }
+            }
+        });
+    }
+
+    removeCrystal() {
+    	if (typeof this.scene !== 'undefined') {
+    		var oldCrystal = this.scene.getObjectByName('crystal');
+			this.scene.remove(oldCrystal);
+			StructureViewer.disposeNode(oldCrystal);
+    	}
+    }
+
+    replaceCrystal(crystal_json) {
+
+		if (typeof this.scene !== 'undefined') {
+            this.removeCrystal();
+			this.crystal = StructureViewer.makeCrystal(crystal_json);
+			this.scene.add(this.crystal);
+			this.available_polyhedra = crystal_json.polyhedra.polyhedra_types;
+		}
+
+    }
+
+    start() {
+		if (!this.frameId) {
+			this.frameId = requestAnimationFrame(this.animate)
+		}
+	}
+
+	stop() {
+		cancelAnimationFrame(this.frameId)
+	}
+
+	animate() {
+		this.crystal.rotation.y += this.rotationSpeed;
+		this.renderScene()
+		this.frameId = window.requestAnimationFrame(this.animate)
+	}
+
+	renderScene() {
+		this.renderer.render(this.scene, this.camera)
+	}
+
+}
--- src/components/StructureViewerComponent.react.js
+++ src/components/StructureViewerComponent.react.js
@@ -4,6 +4,7 @@ import React, {
 import PropTypes from 'prop-types';
 import * as THREE from 'three';
 import OrbitControls from 'orbit-controls-es6';
+import StructureViewer from './StructureViewer.js';
 
 // DEVELOPER NOTE: This is written by a novice JavaScript/React developer,
 // here be dragons.
@@ -17,341 +18,56 @@ import OrbitControls from 'orbit-controls-es6';
  */
 export default class StructureViewerComponent extends Component {
 
-	makeCrystal(crystal_json) {
-
-		if (typeof crystal_json !== 'undefined') {
-
-
-			const crystal = new THREE.Object3D();
-			crystal.name = 'crystal';
-			crystal.castsShadow = true;
-			crystal.receivesShadow = true;
-
-			const atoms = this.makeAtoms(crystal_json)
-			crystal.add(atoms)
-
-			const bonds = this.makeBonds(crystal_json, atoms)
-			crystal.add(bonds)
-
-			const unit_cell = this.makeUnitCell(crystal_json)
-			crystal.add(unit_cell)
-
-			const polyhedra = this.makePolyhedra(crystal_json, atoms)
-			crystal.add(polyhedra)
-
-			return crystal
-
-		}
-
-	}
-
-	static getMaterial(color) {
-		return new THREE.MeshPhongMaterial({
-			color: color,
-			shininess: 1,
-			reflectivity: 1,
-			specular: 0xffffff
-		});
-	}
-
-	makeAtoms(crystal_json) {
-
-		const scale = 0.5;
-
-		const atoms = new THREE.Object3D();
-		atoms.name = 'atoms';
-
-		crystal_json.atoms.forEach(function(atom) {
-			const radius = atom.fragments[0].radius * scale;
-			const geometry = new THREE.SphereGeometry(radius, 32, 32);
-			const color = new THREE.Color(...atom.fragments[0].color);
-			const material = StructureViewerComponent.getMaterial(color);
-			const sphere = new THREE.Mesh(geometry, material);
-			sphere.position.set(...atom.position)
-			atoms.add(sphere);
-		});
-
-		return atoms;
-	}
-
-	makeBonds(crystal_json, atoms) {
-
-		const bonds = new THREE.Object3D();
-		bonds.name = 'bonds';
-
-		crystal_json.bonds.forEach(function(bond) {
-
-			const sourceObj = atoms.children[bond.from_atom_index]
-			const destinationObj = atoms.children[bond.to_atom_index]
-
-			var midPoint = destinationObj.position.clone().sub(sourceObj.position);
-			const len = midPoint.length();
-			midPoint = midPoint.multiplyScalar(0.5);
-			midPoint = sourceObj.position.clone().add(midPoint)
-
-			const geometry = new THREE.CylinderGeometry(0.1, 0.1, len / 2, 16, 1);
-
-			geometry.applyMatrix(new THREE.Matrix4().makeTranslation(0, len / 4, 0));
-			geometry.applyMatrix(new THREE.Matrix4().makeRotationX(Math.PI / 2));
-
-			const material_from = StructureViewerComponent.getMaterial(atoms.children[bond.from_atom_index].material.color);
-
-			const cylinder_from = new THREE.Mesh(geometry, material_from);
-
-			cylinder_from.translateX(sourceObj.position.x);
-			cylinder_from.translateY(sourceObj.position.y);
-			cylinder_from.translateZ(sourceObj.position.z);
-
-			cylinder_from.lookAt(midPoint);
-
-			const material_to = StructureViewerComponent.getMaterial(atoms.children[bond.to_atom_index].material.color);
-
-			const cylinder_to = new THREE.Mesh(geometry, material_to);
-			cylinder_to.translateX(destinationObj.position.x);
-			cylinder_to.translateY(destinationObj.position.y);
-			cylinder_to.translateZ(destinationObj.position.z);
-
-			cylinder_to.lookAt(midPoint);
-
-			bonds.add(cylinder_from);
-			bonds.add(cylinder_to);
-
-		})
-
-		return bonds;
-
-	}
-
-	makeUnitCell(crystal_json) {
-
-		const edges = new THREE.Geometry();
-		crystal_json.unit_cell.lines.map(p => edges.vertices.push(new THREE.Vector3(...p)))
-
-		const unitcell = new THREE.LineSegments(edges,
-			new THREE.LineBasicMaterial({
-				color: 0x0,
-				linewidth: 1
-			}));
-		unitcell.name = 'unitcell';
-
-		return unitcell
-	}
-
-	makePolyhedra(crystal_json, atoms) {
-
-		const polyhedra = new THREE.Object3D();
-		polyhedra.name = 'polyhedra';
-		
-		this.available_polyhedra = crystal_json.polyhedra.polyhedra_types
-		this.default_polyhedra = crystal_json.polyhedra.default_polyhedra_types
-
-		for (var polyhedron_type in crystal_json.polyhedra.polyhedra_by_type) {
-
-		    const polyhedra_type_object = new THREE.Object3D();
-		    polyhedra_type_object.name = polyhedron_type;
-
-		    crystal_json.polyhedra.polyhedra_by_type[polyhedron_type].forEach(function(polyhedron) {
-
-			    const polyhedron_geometry = new THREE.Geometry();
-			    polyhedron.points.map(p => polyhedron_geometry.vertices.push(new THREE.Vector3(...p)))
-			    polyhedron.hull.map(p => polyhedron_geometry.faces.push(new THREE.Face3(...p)))
-			    polyhedron_geometry.computeFaceNormals();
-
-			    const polyhedron_color = atoms.children[polyhedron.center].material.color;
-			    const polyhedron_material = StructureViewerComponent.getMaterial(polyhedron_color);
-                polyhedron_material.side = THREE.DoubleSide;
-
-			    const polyhedron_object = new THREE.Mesh(polyhedron_geometry, polyhedron_material)
-
-			    polyhedra_type_object.add(polyhedron_object)
-
-			})
-
-            polyhedra.add(polyhedra_type_object);
-
-		}
-
-		return polyhedra
-
-	}
-
-	disposeNode(parentObject) {
-	// https://stackoverflow.com/questions/33152132/
-        parentObject.traverse(function (node) {
-            if (node instanceof THREE.Mesh) {
-                if (node.geometry) {
-                    node.geometry.dispose();
-                }
-                if (node.material) {
-                    var materialArray;
-                    if (node.material instanceof THREE.MeshFaceMaterial || node.material instanceof THREE.MultiMaterial) {
-                        materialArray = node.material.materials;
-                    }
-                    else if(node.material instanceof Array) {
-                        materialArray = node.material;
-                    }
-                    if(materialArray) {
-                        materialArray.forEach(function (mtrl, idx) {
-                            if (mtrl.map) mtrl.map.dispose();
-                            if (mtrl.lightMap) mtrl.lightMap.dispose();
-                            if (mtrl.bumpMap) mtrl.bumpMap.dispose();
-                            if (mtrl.normalMap) mtrl.normalMap.dispose();
-                            if (mtrl.specularMap) mtrl.specularMap.dispose();
-                            if (mtrl.envMap) mtrl.envMap.dispose();
-                            mtrl.dispose();
-                        });
-                    }
-                    else {
-                        if (node.material.map) node.material.map.dispose();
-                        if (node.material.lightMap) node.material.lightMap.dispose();
-                        if (node.material.bumpMap) node.material.bumpMap.dispose();
-                        if (node.material.normalMap) node.material.normalMap.dispose();
-                        if (node.material.specularMap) node.material.specularMap.dispose();
-                        if (node.material.envMap) node.material.envMap.dispose();
-                        node.material.dispose();
-                    }
-                }
-            }
-        });
-    }
-
-
 	constructor(props) {
 		super(props)
-
-		this.start = this.start.bind(this)
-		this.stop = this.stop.bind(this)
-		this.animate = this.animate.bind(this)
 	}
 
 	componentDidMount() {
 
-		// This is where all the Three.js scene construction happens
-
-		const width = this.mount.clientWidth
-		const height = this.mount.clientHeight
-
-		const scene = new THREE.Scene()
-		const camera = new THREE.OrthographicCamera(
-			width / -2,
-			width / 2,
-			height / 2,
-			height / -2, -2000, 2000);
-
-		camera.position.z = 2;
-
-		const renderer = new THREE.WebGLRenderer({
-			antialias: true,
-			alpha: true
-		})
-
-		renderer.setPixelRatio(window.devicePixelRatio * 1.5);
-		renderer.setClearColor(0xffffff, 0)
-		renderer.setSize(width, height)
-
-		// Lighting
-
-		const ambientLight = new THREE.AmbientLight(0xffffff, 0.002);
-		scene.add(ambientLight)
-
-		const directionalLight = new THREE.DirectionalLight(0xffffff, 0.0015);
-		directionalLight.position.set(-1, 1, 1).normalize();
-		scene.add(directionalLight);
-
-		const hemisphereLight = new THREE.HemisphereLight(0xffffff, 0x222222, 0.0015);
-		scene.add(hemisphereLight)
-
-		//const pointLight = new THREE.PointLight(0xffffff, 0.05);
-		//camera.add(pointLight)
-        //const pointLightHelper = new THREE.PointLightHelper( pointLight, 1, 0xff0000 );
-        //scene.add( pointLightHelper );
-
-		scene.add(camera)
-
-		if (typeof this.props.data !== 'undefined') {
-
-			const crystal = this.makeCrystal(this.props.data);
-
-			scene.add(crystal);
-			camera.lookAt(crystal.position);
-			directionalLight.target = crystal;
-
-			const box = new THREE.Box3();
-			box.setFromObject(crystal);
-			camera.zoom = Math.min(width / (box.max.x - box.min.x),
-				height / (box.max.y - box.min.y)) * 0.6;
-			camera.updateProjectionMatrix();
-			camera.updateMatrix();
-
-			this.crystal = crystal;
-		}
-
-		this.scene = scene
-		this.camera = camera
-		this.renderer = renderer
-
-		// Controls
-		const controls = new OrbitControls(camera, this.renderer.domElement);
-
-		this.mount.appendChild(this.renderer.domElement)
-		this.start();
+	    this.structure_viewer = new StructureViewer(this.props.data, this.mount)
+	    this.rotationSpeed = this.props.rotationSpeed
 
 	}
 
 	componentWillUpdate(nextProps, nextState) {
 
 		if (nextProps.data !== this.props.data) {
-			if (typeof this.scene !== 'undefined') {
-				var oldCrystal = this.scene.getObjectByName('crystal');
-				this.scene.remove(oldCrystal);
-				this.disposeNode(oldCrystal);
-				this.crystal = this.makeCrystal(nextProps.data);
-				this.scene.add(this.crystal);
-			}
+		    this.structure_viewer.replaceCrystal(nextProps.data);
 		}
 
-		if (typeof this.crystal !== 'undefined') {
-			var all_options = ['atoms', 'bonds', 'unitcell', 'polyhedra']
-			if (typeof this.available_polyhedra !== 'undefined') {
-				all_options.push(...this.available_polyhedra)
-			}
-			const crystal = this.crystal
+		if (typeof this.structure_viewer !== 'undefined') {
+			//var all_options = ['atoms', 'bonds', 'unitcell', 'polyhedra']
+			//if (typeof this.available_polyhedra !== 'undefined') {
+			//	all_options.push(...this.available_polyhedra)
+			//}
+			//const crystal = this.crystal
 			if (nextProps.visibilityOptions != this.props.visibilityOptions) {
-				all_options.forEach(function(option) {
-					var object = crystal.getObjectByName(option);
-					if (typeof object !== "undefined") {
-						object.visible = nextProps.visibilityOptions.includes(option)
-					}
-				})
+			    this.structure_viewer.changeVisibility(nextProps.visibilityOptions)
 			}
+			//	all_options.forEach(function(option) {
+			//		var object = crystal.getObjectByName(option);
+			//		if (typeof object !== "undefined") {
+			//			object.visible = nextProps.visibilityOptions.includes(option)
+			//		}
+			//	})
+			//}
+		}
+
+		if (nextProps.n_screenshot_requests != this.props.n_screenshot_requests) {
+		    // this is correct in React?
+		    this.props.screenshot = this.structure_viewer.renderer.domElement.toDataURL();
+		}
+
+		if (nextProps.rotationSpeed != this.structure_viewer.rotationSpeed) {
+		    this.structure_viewer.rotationSpeed = nextProps.rotationSpeed;
 		}
 
 	}
 
 	componentWillUnmount() {
-		this.stop()
-		this.mount.removeChild(this.renderer.domElement)
-	}
-
-	start() {
-		if (!this.frameId) {
-			this.frameId = requestAnimationFrame(this.animate)
-		}
-	}
-
-	stop() {
-		cancelAnimationFrame(this.frameId)
-	}
-
-	animate() {
-	    const { rotationSpeed } = this.props;
-		this.crystal.rotation.y += rotationSpeed;
-		this.renderScene()
-		this.frameId = window.requestAnimationFrame(this.animate)
-	}
-
-	renderScene() {
-		this.renderer.render(this.scene, this.camera)
+		this.structure_viewer.stop()
+		this.structure_viewer.removeCrystal()
+		this.mount.removeChild(this.structure_viewer.renderer.domElement)
 	}
 
 	render() {
@@ -381,7 +97,8 @@ export default class StructureViewerComponent extends Component {
 }
 
 StructureViewerComponent.defaultProps = {
-    rotationSpeed: 0.001
+    rotationSpeed: 0.001,
+    n_screenshot_requests: 0
 }
 
 StructureViewerComponent.propTypes = {
@@ -401,7 +118,7 @@ StructureViewerComponent.propTypes = {
 	 * (This also includes specific polyhedra tags, might
 	 *  separate this into a separate prop later)
 	 */
-	visibilityOptions: PropTypes.array,
+	visibilityOptions: PropTypes.object,
 
 	/**
 	 * Amount to rotate about y-axis per frame
@@ -413,5 +130,15 @@ StructureViewerComponent.propTypes = {
 	 * Dash-assigned callback that should be called whenever any of the
 	 * properties change
 	 */
-	setProps: PropTypes.func
+	setProps: PropTypes.func,
+
+	/**
+	 * Increment to trigger a screenshot request
+	 */
+	n_screenshot_requests: PropTypes.number,
+
+	/**
+	 * Screenshot as PNG data URI.
+	 */
+	screenshot: PropTypes.string
 };
