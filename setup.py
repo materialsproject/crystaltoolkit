4ec79f4dbf69f8214564dc2da09f8843a899fdde
Matthew Horton <mkhorton@lbl.gov>
2018-10-04 14:07:33 -0700

    Update setup.py for pypi

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 exec (open('mp_dash_components/version.py').read())
 
@@ -6,9 +6,9 @@ setup(
     name='mp_dash_components',
     version=__version__,
     author='mkhorton',
-    packages=['mp_dash_components'],
+    packages=find_packages(),
     include_package_data=True,
     license='MIT',
-    description='Component for viewing crystallographic structures for the Materials Project.',
-    install_requires=[]
+    description='Dash components for viewing Materials Project-specific objects such as crystallographic structures.',
+    install_requires=["pymatgen>=2018.9.19", "dash>=0.26.4", ]
 )
