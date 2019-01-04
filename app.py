f323c05f8bfc74451aa2bda4c777e95b8d540dc9
Matthew Horton <mkhorton@lbl.gov>
2019-01-03 21:14:00 -0800

    Clean up app.py

--- app.py
+++ app.py
@@ -4,47 +4,85 @@ import dash_html_components as html
 
 from dash.dependencies import Input, Output, State
 from dash.exceptions import PreventUpdate
-from uuid import uuid4
+
+import os
 
 from flask import make_response, jsonify, request
+from flask_caching import Cache
 
-import mp_dash_components
-
-from mp_dash_components.components.structure import StructureMoleculeComponent
+from mp_dash_components.components.core import MPComponent
 from mp_dash_components.helpers.layouts import *
 import mp_dash_components as mpc
-from pymatgen import Structure, MPRester, Molecule
+
+from pymatgen import MPRester, Structure, Molecule
+from pymatgen.analysis.graphs import StructureGraph, MoleculeGraph
 from pymatgen import __version__ as pmg_version
 
 from json import loads
+from uuid import uuid4
 
-struct = Structure([[1.5, 0, 0], [0, 1.5, 0], [0, 0, 1.5]], ["N"], [[0, 0, 0]])
-struct = struct * (6, 6, 6)
-# struct.remove_sites([2,3,4,5,6,7,8])
-##struct.replace(0, "C")
-# struct.replace(1, "C")
-##struct.replace(2, "C")
-##struct.replace(3, "C")
-struct = MPRester().get_structure_by_material_id("mp-510587")
-mol = Molecule(["C", "H"], [[0, 0, 0], [1.2, 0, 0]])
-#struct = mol
-#struct = None
+
+################################################################################
+# region SET UP APP
+################################################################################
 
 app = dash.Dash()
-app.config['suppress_callback_exceptions']=True
+app.config["suppress_callback_exceptions"] = True
 app.title = "Crystal Toolkit"
-
 app.scripts.config.serve_locally = True
+app.server.secret_key = str(uuid4())  # TODO: will need to change this one day
+server = app.server
 
-struct_component = StructureMoleculeComponent(struct, app=app)
-download_component = (mpc.Section([Icon(), struct_component.layouts["screenshot"]]),)
+MPComponent.register_app(app)
+
+# endregion
+##########
+
+
+################################################################################
+# region SET UP CACHE
+################################################################################
+
+try:
+    cache = Cache(
+        app.server,
+        config={
+            "CACHE_TYPE": "redis",
+            "CACHE_REDIS_URL": os.environ.get("REDIS_URL", ""),
+        },
+    )
+except Exception as exception:
+    app.logger.error(
+        f"Failed to connect to Redis cache, falling back to "
+        f"file system cache: {exception}"
+    )
+    cache = Cache(app.server, config={"CACHE_TYPE": "filesystem"})
+
+MPComponent.register_cache(cache)
+
+# endregion
+
+
+################################################################################
+# region INSTANTIATE CORE COMPONENTS
+################################################################################
+
+struct_component = mpc.StructureMoleculeComponent()
+search = mpc.SearchComponent()
+editor = mpc.JSONComponent()
+
+# endregion
+
+################################################################################
+# region CREATE OTHER LAYOUT ELEMENTS
+################################################################################
 
 footer = mpc.Footer(
     html.Div(
         dcc.Markdown(
             f"In beta. Bug reports and feature requests gratefully accepted, "
             f"contact [@mkhorton](mailto:mkhorton@lbl.gov).  \n"
-            f"Web app created by [Crystal Toolkit Development Team]"
+            f"Created by [Crystal Toolkit Development Team]"
             f"(https://github.com/materialsproject/mash/graphs/contributors), "
             f"powered by [The Materials Project](https://materialsproject.org) "
             f"and [pymatgen](http://pymatgen.org) v{pmg_version}."
@@ -53,6 +91,12 @@ footer = mpc.Footer(
     ),
     style={"padding": "1rem 1rem 1rem", "background-color": "inherit"},
 )
+
+# endregion
+
+################################################################################
+# region  DEFINE MAIN LAYOUT
+################################################################################
 
 app.layout = Container(
     [
@@ -63,22 +107,53 @@ app.layout = Container(
                 html.Div(
                     struct_component.layouts["struct"],
                     style={
-                        "width": "500px",
-                        "height": "500px",
+                        "width": "65vmin",
+                        "height": "65vmin",
+                        "min-width": "200px",
+                        "min-height": "200px",
                         "overflow": "hidden",
                         "padding": "0.25rem",
                     },
                     className="box",
                 ),
+                html.Div(struct_component.layouts["screenshot"]),
+                search.all_layouts,
             ]
         ),
-        struct_component.layouts["screenshot"],
         # Section(html.Details([html.Summary(html.A("Click here", className="button")), html.Div("Test!", className="box")]))
         footer,
-        struct_component._store
+        struct_component._store,
     ]
 )
 
+# endregion
+
+################################################################################
+# region SET UP API ROUTES (to support creating viewer links)
+################################################################################
+
+
+@server.route("/version", methods=["GET"])
+def get_version():
+    return make_response(
+        jsonify(
+            {
+                "crystal_toolkit_version": 1,
+                "crystal_toolkit_api_version": 1,
+                "pymatgen_version": pmg_version,
+            }
+        )
+    )
+
+
+@server.route("/generate_token", methods=["POST"])
+def get_token():
+    print(str(request.json))
+    token = mson_to_token(request.json, cache)
+    if token["error"] is None:
+        return make_response(jsonify(token), 200)
+    else:
+        return make_response(jsonify(token), 403)
 
 
 def mson_to_token(mson, cache):
@@ -86,7 +161,7 @@ def mson_to_token(mson, cache):
     # sanity check
     allowed_classes = [Structure, Molecule, StructureGraph, MoleculeGraph]
     allowed_mson = False
-    if len(mson) > 1024*1024:
+    if len(mson) > 1024 * 1024:
         # set a sensible size limit
         return {"token": None, "error": "Request too large."}
 
@@ -102,7 +177,8 @@ def mson_to_token(mson, cache):
         return {"token": None, "error": "Format not recognized."}
 
     token = str(uuid4())[0:6]
-    cache.set(token, mson, timeout=604800, key_prefix="crystal_toolkit_user_")
+    # set to 1 week expiration by default
+    cache.set(token, mson, timeout=604_800, key_prefix="crystal_toolkit_user_")
     return token
 
 
@@ -110,25 +186,18 @@ def token_to_mson(token, cache):
     return cache.get(token)
 
 
-app.server.secret_key = str(uuid4())
-server = app.server
+# endregion
+
+################################################################################
+# region SET UP CALLBACKS
+################################################################################
 
 
-@server.route('/version', methods=['GET'])
-def get_version():
-    return make_response(jsonify({'crystal_toolkit_version': 1,
-                                  'crystal_toolkit_api_version': 1,
-                                  'pymatgen_version': pmg_version}))
+# endregion
 
-@server.route('/generate_token', methods=['POST'])
-def get_token():
-    print(str(request.json))
-    token = mson_to_token(request.json, cache)
-    if token["error"] is None:
-        return make_response(jsonify(token), 200)
-    else:
-        return make_response(jsonify(token), 403)
-
+################################################################################
+# Run server :-)
+################################################################################
 
 if __name__ == "__main__":
-    app.run_server(debug=True, port=8082)
+    app.run_server(debug=False, port=8080)
