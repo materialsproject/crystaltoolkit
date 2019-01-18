b9f17ec0e975e43aec7da860397780fadcb17c3c
Matthew Horton <mkhorton@lbl.gov>
2019-01-17 20:49:54 -0800

    Add Upload component

--- crystal_toolkit/__init__.py
+++ crystal_toolkit/__init__.py
@@ -60,6 +60,7 @@ from crystal_toolkit.components.magnetism import MagnetismComponent
 from crystal_toolkit.components.bonding_graph import BondingGraphComponent
 from crystal_toolkit.components.magnetism import MagnetismComponent
 from crystal_toolkit.components.transformations.core import TransformationsComponent
+from crystal_toolkit.components.upload import StructureMoleculeUploadComponent
 #from crystal_toolkit.components.summary import SummaryComponent
 from crystal_toolkit.helpers.layouts import *
 from crystal_toolkit.helpers.scene import *
