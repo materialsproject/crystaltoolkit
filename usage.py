f8925bc2942ec1b924bf3ef20f7c38a61d54dda0
Matthew Horton <mkhorton@lbl.gov>
2019-01-25 15:24:29 -0800

    Commit example usage for adding objects to scene

--- usage.py
+++ usage.py
@@ -2,11 +2,13 @@ import dash
 import dash_html_components as html
 import crystal_toolkit as ct
 
+from crystal_toolkit.helpers.scene import *
+
 from pymatgen import Structure, Lattice
 
 app = dash.Dash(__name__)
 
-app.config['suppress_callback_exceptions']=True
+app.config["suppress_callback_exceptions"] = True
 app.scripts.config.serve_locally = True
 app.css.config.serve_locally = True
 app.title = "Crystal Toolkit Example Components"
@@ -21,7 +23,15 @@ example_struct = Structure.from_spacegroup(
     ["Ga", "N"],
     [[1 / 3, 2 / 3, 0], [1 / 3, 2 / 3, 3 / 8]],
 )
-struct_component = ct.StructureMoleculeComponent(example_struct)
+
+test_scene = [Scene("test", contents=[
+    Spheres(positions=[[0, 0, 0]]),
+    Surface(positions=[[0, 10, 0], [10, 0, 0], [0, 0, 10]])
+])]
+
+struct_component = ct.StructureMoleculeComponent(
+    example_struct, scene_additions=test_scene
+)
 
 app.layout = html.Div(struct_component.standard_layout)
 
