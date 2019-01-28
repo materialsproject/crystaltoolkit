2f2e5574c87e0f48a6904e96e31371ae8acd4ab3
JSX <jmmshn@gmail.com>
2019-01-28 15:04:18 -0800

    working surfaces

--- usage.py
+++ usage.py
@@ -1,10 +1,15 @@
 import dash
 import dash_html_components as html
 import crystal_toolkit as ct
+import numpy as np
 
 from crystal_toolkit.helpers.scene import *
 
 from pymatgen import Structure, Lattice
+from pymatgen.io.vasp import Chgcar
+
+from skimage import measure
+
 
 app = dash.Dash(__name__)
 
@@ -24,13 +29,28 @@ example_struct = Structure.from_spacegroup(
     [[1 / 3, 2 / 3, 0], [1 / 3, 2 / 3, 3 / 8]],
 )
 
+# get the data points to plot
+def get_mesh(chgcar, data_tag='total', isolvl=2.0, step_size = 4):
+    vertices, faces, normals, values = measure.marching_cubes_lewiner(chgcar.data[data_tag],
+                                                                      level=isolvl,
+                                                                      step_size=step_size)
+    vertices = vertices/chgcar.data[data_tag].shape  # transform to fractional coordinates
+    vertices = np.dot(vertices-0.5, cc.structure.lattice.matrix) # transform to cartesian
+    return vertices, faces
+
+cc = Chgcar.from_file('./chg.vasp')
+vertices,faces = get_mesh(cc)
+vertices = vertices
+pos = [vert for triangle in vertices[faces].tolist() for vert in triangle]
+
+
 test_scene = [Scene("test", contents=[
-    Spheres(positions=[[0, 0, 0]]),
-    Surface(positions=[[0, 10, 0], [10, 0, 0], [0, 0, 10]])
+    Surface(positions=pos),
+    Cubes(positions=[[0,0,0]])
 ])]
 
 struct_component = ct.StructureMoleculeComponent(
-    example_struct, scene_additions=test_scene
+    cc.structure, scene_additions=test_scene, hide_incomplete_bonds=True
 )
 
 app.layout = html.Div(struct_component.standard_layout)
