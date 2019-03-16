172e38f12b663a99d3a9fdbe7d7fa43d97e4370b
Matthew Horton <mkhorton@lbl.gov>
2019-03-15 17:07:26 -0700

    Add test Jupyter support

--- crystal_toolkit/helpers/jupyter.py
+++ crystal_toolkit/helpers/jupyter.py
@@ -4,19 +4,26 @@ import dash
 import warnings
 import dash_html_components as html
 
+from monty.json import MSONable
+from pymatgen.core.structure import Structure, Molecule
+
+from crystal_toolkit import JSONViewComponent
 from crystal_toolkit.components.structure import StructureMoleculeComponent
 
 
 def init_viewer():
 
-    global viewer
-    global app
+    global crystal_toolkit_viewer
+    global crystal_toolkit_app
 
-    viewer = jupyterlab_dash.AppViewer(port=8090)
-    app = dash.Dash(__name__)
+    crystal_toolkit_viewer = jupyterlab_dash.AppViewer(port=8090)
+    crystal_toolkit_app = dash.Dash(__name__)
 
 
 def view(struct_or_mol):
+
+    if 'crystal_toolkit_app' not in globals():
+        init_viewer()
 
     with warnings.catch_warnings():
         warnings.simplefilter("ignore")
@@ -25,6 +32,17 @@ def view(struct_or_mol):
                                                color_scheme='VESTA',
                                                radius_strategy='uniform')
 
-    app.title = struct_or_mol.composition.reduced_formula
-    app.layout = html.Div([component.standard_layout])
-    viewer.show(app)
+    crystal_toolkit_app.title = struct_or_mol.composition.reduced_formula
+    crystal_toolkit_app.layout = html.Div([component.standard_layout])
+    crystal_toolkit_viewer.show(crystal_toolkit_app)
+
+
+def get_component(obj):
+
+    if not isinstance(obj, MSONable):
+        raise ValueError("Can only display MSONable types.")
+
+    if isinstance(obj, Structure) or isinstance(obj, Molecule):
+        return StructureMoleculeComponent
+
+    return JSONViewComponent(src=obj.to_json())
