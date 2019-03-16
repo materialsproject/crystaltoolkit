10304a511d9e17e5a28d7a2d6501887ce1d3e505
JSX <jmmshn@gmail.com>
2019-03-15 18:50:32 -0700

    changed back to simpler usage, the charge density stuff should be tested elsewhere

--- usage.py
+++ usage.py
@@ -1,13 +1,8 @@
 import dash
 import dash_html_components as html
-import crystal_toolkit as ct
-import numpy as np
+import crystal_toolkit.components as ctc
 
 from pymatgen import Structure, Lattice
-from pymatgen.io.vasp import Chgcar
-
-from skimage import measure
-
 
 app = dash.Dash(__name__)
 
@@ -29,28 +24,9 @@ example_struct = Structure.from_spacegroup(
     [[1 / 3, 2 / 3, 0], [1 / 3, 2 / 3, 3 / 8]],
 )
 
-def get_mesh(chgcar, data_tag='total', isolvl=2.0, step_size = 4):
-    vertices, faces, normals, values = measure.marching_cubes_lewiner(chgcar.data[data_tag],
-                                                                      level=isolvl,
-                                                                      step_size=step_size)
-    vertices = vertices/chgcar.data[data_tag].shape  # transform to fractional coordinates
-    vertices = np.dot(vertices-0.5, cc.structure.lattice.matrix) # transform to cartesian
-    return vertices, faces
-
-cc = Chgcar.from_file('./chg.vasp')
-vertices,faces = get_mesh(cc)
-vertices = vertices
-pos = [vert for triangle in vertices[faces].tolist() for vert in triangle]
-
-
-test_scene = [Scene("test", contents=[
-    Surface(positions=pos),
-    Cubes(positions=[[0,0,0]])
-])]
-
-struct_component = ct.StructureMoleculeComponent(
-    cc.structure, scene_additions=test_scene, hide_incomplete_bonds=True
-# get the data points to plot
+# instantiate a component to view structures
+struct_component = ctc.StructureMoleculeComponent(
+    example_struct  # this is a pymatgen Structure
 )
 
 # for a custom-sized component, use `struct_component.struct_layout` and put
