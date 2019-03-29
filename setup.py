d6800c46ffc4c7de0659497bcb6f5a922235b0af
Donny Winston <dwinston@alum.mit.edu>
2019-03-29 10:18:58 -0700

    Add install_requires entries for 'pip install' to install deps

--- setup.py
+++ setup.py
@@ -8,6 +8,15 @@ with open(os.path.join('crystal_toolkit', 'package.json')) as f:
 
 package_name = package["name"].replace(" ", "_").replace("-", "_")
 
+
+def requirements(fname):
+    with open(fname, 'r') as f:
+        reqs = f.read().splitlines()
+    reqs = [r.strip().split()[0] for r in reqs if r.strip() and not r.strip().startswith('#')]
+    reqs = [r.replace('==', '>=') for r in reqs if '>=' in r or '==' in r]
+    return reqs
+
+
 setup(
     name=package_name,
     version=package["version"],
@@ -16,5 +25,5 @@ setup(
     include_package_data=True,
     license=package['license'],
     description=package['description'] if 'description' in package else package_name,
-    install_requires=[]
+    install_requires=requirements('requirements.txt'),
 )
