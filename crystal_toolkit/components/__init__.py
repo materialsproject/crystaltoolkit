d494c23097b22e944e6c0c293ea72763feb471d9
Matthew Horton <mkhorton@lbl.gov>
2019-04-23 16:24:45 -0700

    Add SubmitSNLPanel import

--- crystal_toolkit/components/__init__.py
+++ crystal_toolkit/components/__init__.py
@@ -16,6 +16,7 @@ from crystal_toolkit.components.bonding_graph import BondingGraphComponent
 from crystal_toolkit.components.magnetism import MagnetismComponent
 from crystal_toolkit.components.xrd import XRayDiffractionComponent, XRayDiffractionPanelComponent
 from crystal_toolkit.components.download import DownloadComponent
+from crystal_toolkit.components.submit_snl import SubmitSNLPanel
 from crystal_toolkit.components.symmetry import SymmetryComponent
 from crystal_toolkit.components.upload import StructureMoleculeUploadComponent
 from crystal_toolkit.components.phase_diagram import PhaseDiagramComponent, PhaseDiagramPanelComponent
