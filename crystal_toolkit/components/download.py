9803301aa44fa54afc9e49fe11f65937a15f7ea3
Matthew Horton <mkhorton@lbl.gov>
2019-05-28 17:34:24 -0700

    Update download component options, remove extra print statements

--- crystal_toolkit/components/download.py
+++ crystal_toolkit/components/download.py
@@ -8,14 +8,11 @@ from crystal_toolkit.helpers.layouts import Button
 
 from base64 import b64encode
 
+
 class DownloadPanelComponent(PanelComponent):
 
     # human-readable label to file extension
-    struct_options = {
-        'CIF': 'cif',
-        'POSCAR': 'poscar',
-        'JSON': 'json'
-    }
+    struct_options = {"CIF": "cif", "POSCAR": "poscar", "JSON": "json"}
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -26,45 +23,34 @@ class DownloadPanelComponent(PanelComponent):
 
     @property
     def description(self):
-        return (
-            "Download your crystal structure or molecule."
-        )
+        return "Download your crystal structure or molecule."
 
     def update_contents(self, new_store_contents):
 
         options = dcc.Dropdown(
             id=self.id("file_extension"),
             value="cif",
-            options=[{'label': k, 'value': v} for k, v in self.struct_options.items()],
-            style={"display": "inline-block"}
+            options=[{"label": k, "value": v} for k, v in self.struct_options.items()],
         )
 
         download_button = Button("Download File", id=self.id("download"))
 
-        return html.Div([
-            options,
-            download_button
-        ])
+        return html.Div([options, download_button])
 
     def _generate_callbacks(self, app, cache):
         super()._generate_callbacks(app, cache)
 
         @app.callback(
             Output(self.id("download"), "children"),
-            [Input(self.id(), "data"),
-             Input(self.id("file_extension"), "value")]
+            [Input(self.id(), "data"), Input(self.id("file_extension"), "value")],
         )
         def update_href(data, file_extension):
 
             structure = self.from_data(data)
             contents = structure.to(fmt=file_extension)
 
-            print(contents)
-            print(file_extension)
-
             base64 = b64encode(contents.encode("utf-8")).decode("ascii")
 
             href = f"data:text/plain;charset=utf-8;base64,{base64}"
 
-            return html.A(f"Download File", href=href,
-                          target="_blank")
+            return html.A(f"Download File", href=href, target="_blank")
