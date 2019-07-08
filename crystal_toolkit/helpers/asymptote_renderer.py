db8afae1ad0d1e148d73a80a5b2a4e4c36a33e6b
Matthew Horton <mkhorton@lbl.gov>
2019-07-08 13:08:31 -0700

    Rename asymptote renderer

--- crystal_toolkit/helpers/asymptote_renderer.py
+++ crystal_toolkit/helpers/asymptote_renderer.py
@@ -75,39 +75,49 @@ draw(IPOS--FPOS, dashed);
 {% endfor %}
 """
 
+
 def asy_write_data(input_scene_comp, fstream):
     """
     parse a primitive display object in crystaltoolkit and print it to asymptote
     input_scene_comp
     fstream
     """
-    if input_scene_comp['type'] == 'spheres':
-        positions = input_scene_comp['positions']
+    if input_scene_comp["type"] == "spheres":
+        positions = input_scene_comp["positions"]
         positions = [tuple(pos) for pos in positions]
-         
-        fstream.write(Environment().from_string(TEMP_SPHERE).render(
-            positions=positions,
-            radius=input_scene_comp['radius'],
-            color=input_scene_comp['color'].replace('#', '')))
-        
-    if input_scene_comp['type'] == 'cylinders':
-        # need to transforme all the cylinders to vector
-        posPairs = [
-           [tuple(ipos), tuple(fpos)] for ipos, fpos in input_scene_comp['positionPairs']
-        ]
-        fstream.write(Environment().from_string(TEMP_CYLINDER).render(
-            posPairs=posPairs, color=input_scene_comp['color'].replace('#', '')))
-        
-    if input_scene_comp['type'] == 'lines':
-        # need to transforme all the cylinders to vector
-        pos1, pos2 = input_scene_comp['positions'][0::2], input_scene_comp['positions'][1::2]
-        posPairs = [
-           [tuple(ipos), tuple(fpos)] for ipos, fpos in zip(pos1, pos2)
-        ]
-        fstream.write(Environment().from_string(TEMP_LINE).render(
-            posPairs=posPairs))
 
-    # TODO Leaving out polyhedra for now since asymptote 
+        fstream.write(
+            Environment()
+            .from_string(TEMP_SPHERE)
+            .render(
+                positions=positions,
+                radius=input_scene_comp["radius"],
+                color=input_scene_comp["color"].replace("#", ""),
+            )
+        )
+
+    if input_scene_comp["type"] == "cylinders":
+        # need to transforme all the cylinders to vector
+        posPairs = [
+            [tuple(ipos), tuple(fpos)]
+            for ipos, fpos in input_scene_comp["positionPairs"]
+        ]
+        fstream.write(
+            Environment()
+            .from_string(TEMP_CYLINDER)
+            .render(posPairs=posPairs, color=input_scene_comp["color"].replace("#", ""))
+        )
+
+    if input_scene_comp["type"] == "lines":
+        # need to transforme all the cylinders to vector
+        pos1, pos2 = (
+            input_scene_comp["positions"][0::2],
+            input_scene_comp["positions"][1::2],
+        )
+        posPairs = [[tuple(ipos), tuple(fpos)] for ipos, fpos in zip(pos1, pos2)]
+        fstream.write(Environment().from_string(TEMP_LINE).render(posPairs=posPairs))
+
+    # TODO Leaving out polyhedra for now since asymptote
     # does not have an easy way to generate convex polyhedra from the points
     # Need to write a python conversion between Convex type and surfaces to make this work.
 
@@ -116,21 +126,23 @@ def asy_write_data(input_scene_comp, fstream):
     # at each vertex in the unit cell, we can evaluate the sum of all three lattice vectors from the point
     # then the <vec_sum | vec_to_camera> for each vertex.  The smallest normalized vertex contians the three lines that should be dashed
 
+
 def filter_data(scene_data, fstream):
     """
     Recursively traverse the scene_data dictionary to find objects to draw
     """
-    if 'type' in scene_data.keys():
+    if "type" in scene_data.keys():
         asy_write_data(scene_data, fstream)
     else:
-        for itr in scene_data['contents']:
+        for itr in scene_data["contents"]:
             filter_data(itr, fstream)
 
-def write_asy_file(smc , file_name):
+
+def write_asy_file(smc, file_name):
     """
     smc : (StructureMoleculeComponent)
     """
-    fstream = open(file_name, 'w')
+    fstream = open(file_name, "w")
     fstream.write(HEAD)
     filter_data(smc.initial_scene_data, fstream)
     fstream.close()
