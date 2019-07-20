4a70f02f73ade77c51d7ea51e3313b5d936fd7c1
Shyam Dwaraknath <shyamd@lbl.gov>
2019-07-19 22:32:20 -0700

    move ray casting to Simple3DScene

--- src/lib/components/Simple3DSceneComponent.react.js
+++ src/lib/components/Simple3DSceneComponent.react.js
@@ -1,8 +1,8 @@
 import React, {
-	Component
-} from 'react';
-import PropTypes from 'prop-types';
-import Simple3DScene from './Simple3DScene.js';
+  Component
+} from 'react'
+import PropTypes from 'prop-types'
+import Simple3DScene from './Simple3DScene.js'
 
 /**
  * Simple3DSceneComponent is intended to draw simple 3D scenes using the popular
@@ -12,139 +12,120 @@ import Simple3DScene from './Simple3DScene.js';
  * inside Python to make this component redundant.
  */
 export default class Simple3DSceneComponent extends Component {
+  constructor (props) {
+    super(props)
+    this.handleClick = this.handleClick.bind(this) // Binding here
+  }
 
-	constructor(props) {
-		super(props)
-	}
+  componentDidMount () {
+    this.scene = new Simple3DScene(this.props.data, this.mount, this.props.settings)
+    this.scene.toggleVisibility(this.props.toggleVisibility)
+  }
 
-	componentDidMount() {
+  componentWillUpdate (nextProps, nextState) {
+    if (nextProps.downloadRequest !== this.props.downloadRequest) {
+      this.scene.download(nextProps.downloadRequest.filename, nextProps.downloadRequest.filetype)
+    }
 
-	    this.scene = new Simple3DScene(this.props.data, this.mount, this.props.settings);
-	    this.scene.toggleVisibility(this.props.toggleVisibility);
+    if (nextProps.data !== this.props.data) {
+      this.scene.addToScene(nextProps.data)
+      this.scene.toggleVisibility(this.props.toggleVisibility)
+    }
 
-	}
+    if (nextProps.toggleVisibility !== this.props.toggleVisibility) {
+      this.scene.toggleVisibility(nextProps.toggleVisibility)
+    }
+  }
 
-	componentWillUpdate(nextProps, nextState) {
+  componentWillUnmount () {
+    this.scene.stop()
+    this.mount.removeChild(this.scene.renderer.domElement)
+    this.scene.renderer.forceContextLoss()
+    this.scene.renderer.context = null
+    this.scene.renderer.domElement = null
+    this.scene.renderer = null
+  }
 
-		if (nextProps.downloadRequest !== this.props.downloadRequest) {
-		    this.scene.download(nextProps.downloadRequest.filename, nextProps.downloadRequest.filetype)
-		}
+  handleClick (event) {
+    event.preventDefault()
 
-		if (nextProps.data !== this.props.data) {
-		    this.scene.addToScene(nextProps.data);
-			this.scene.toggleVisibility(this.props.toggleVisibility)
-		}
+    clickedReference = this.scene.getClickedReference(event.clientX, event.clientY)
 
-		if (nextProps.toggleVisibility !== this.props.toggleVisibility) {
-			this.scene.toggleVisibility(nextProps.toggleVisibility)
-		}
+    if (this.props.selectedObjectReference == clickedReference) {
+      this.setState((state) => {
+        return { selectedObjectCount: state.selectedObjectCount + 1 }
+      })
+    } else {
+      this.setState({
+        selectedObjectReference: clickedReference,
+        selectedObjectCount: 1
+      })
+    }
+  }
 
-	}
+  render () {
+    const {
+      id
+    } = this.props
 
-	componentWillUnmount() {
-	    this.scene.stop();
-		this.mount.removeChild(this.scene.renderer.domElement);
-		this.scene.renderer.forceContextLoss();
-		this.scene.renderer.context = null;
-		this.scene.renderer.domElement = null;
-		this.scene.renderer = null;
-
-	}
-
-	render() {
-		const {
-			id
-		} = this.props;
-
-		return ( <div id={id}
-		style = {
-				{
-					'width': '100%',
-					'height': '100%'
-				}
-			}
-			ref = {
-				(mount) => {
-					this.mount = mount
-				}
-			} 
-			onClick = (event) => {
-				event.preventDefault();
-			    
-			    var mouse = new THREE.Vector2();
-			    var renderer = this.scene.renderer
-			    var raycaster = new THREE.Raycaster();
-
-        		mouse.x = (event.clientX / renderer.domElement.clientWidth) * 2 - 1;
-        		mouse.y =  - (event.clientY / renderer.domElement.clientHeight) * 2 + 1;
- 
-        		raycaster.setFromCamera(mouse, camera);
- 
-        		meshObjects = Object.this.scene.clickable_objects; // three.js objects with click handlers we are interested in
-         
-        		var intersects = raycaster.intersectObjects(meshObjects);
- 
-        		if (intersects.length > 0) {
-            		clicked_reference = intersects[0].object.reference;
-            		if (selectedObject.reference == clicked_reference) {
-						this.setState((state) => {
-							return {selectedObjectCount: state.selectedObjectCount + 1};
-						});
-            		} else {
-						this.setState({
-							selectedObjectReference: clicked_reference
-							selectedObjectCount: 1
-						});
-            		}
-        		}
-        	} >
-        	</div>
-        );
-	}
+    return (<div id={id}
+      style={
+        {
+          width: '100%',
+          height: '100%'
+        }
+      }
+      ref={
+        (mount) => {
+          this.mount = mount
+        }
+      }
+      onClick={this.handleClick}
+    />
+    )
+  }
 }
 
-
-
 Simple3DSceneComponent.propTypes = {
-	/**
-	 * The ID used to identify this component in Dash callbacks
-	 */
-	id: PropTypes.string,
+  /**
+         * The ID used to identify this component in Dash callbacks
+         */
+  id: PropTypes.string,
 
-	/**
-	 * Simple3DScene JSON
-	 */
-	data: PropTypes.object,
+  /**
+         * Simple3DScene JSON
+         */
+  data: PropTypes.object,
 
-	/**
-	 * Options used for generating scene
-	 */
-	settings: PropTypes.object,
+  /**
+         * Options used for generating scene
+         */
+  settings: PropTypes.object,
 
-    /**
-	 * Hide/show nodes in scene by name (key), value is 1 to show the node
-	 * and 0 to hide it
-     */
-    toggleVisibility: PropTypes.object,
+  /**
+         * Hide/show nodes in scene by name (key), value is 1 to show the node
+         * and 0 to hide it
+         */
+  toggleVisibility: PropTypes.object,
 
-    /**
-	 * Increment to trigger a screenshot or scene download.
-     */
-    downloadRequest: PropTypes.object,
+  /**
+         * Increment to trigger a screenshot or scene download.
+         */
+  downloadRequest: PropTypes.object,
 
-	/**
-	 * Dash-assigned callback that should be called whenever any of the
-	 * properties change
-	 */
-	setProps: PropTypes.func,
-    /**
-     * Reference to selected objects when clicked
-     */
-    selectedObjectReference: PropTypes.string,
+  /**
+         * Dash-assigned callback that should be called whenever any of the
+         * properties change
+         */
+  setProps: PropTypes.func,
+  /**
+         * Reference to selected objects when clicked
+         */
+  selectedObjectReference: PropTypes.string,
 
-    /**
-     * Click count for selected object
-     */
-    selectedObjectCount: PropTypes.number,
+  /**
+         * Click count for selected object
+         */
+  selectedObjectCount: PropTypes.number
 
-};
+}
