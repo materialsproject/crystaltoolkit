633292e6585b84f28eec91a11b6f8918eeba7773
Matthew Horton <mkhorton@lbl.gov>
2019-07-29 10:49:27 -0700

    Add convenience import

--- crystal_toolkit/__init__.py
+++ crystal_toolkit/__init__.py
@@ -10,19 +10,22 @@ import dash as _dash
 from ._imports_ import *
 from ._imports_ import __all__
 
-if not hasattr(_dash, 'development'):
-    print('Dash was not successfully imported. '
-          'Make sure you don\'t have a file '
-          'named \n"dash.py" in your current directory.', file=_sys.stderr)
+if not hasattr(_dash, "development"):
+    print(
+        "Dash was not successfully imported. "
+        "Make sure you don't have a file "
+        'named \n"dash.py" in your current directory.',
+        file=_sys.stderr,
+    )
     _sys.exit(1)
 
 _basepath = _os.path.dirname(__file__)
-_filepath = _os.path.abspath(_os.path.join(_basepath, 'package.json'))
+_filepath = _os.path.abspath(_os.path.join(_basepath, "package.json"))
 with open(_filepath) as f:
     package = json.load(f)
 
-package_name = package['name'].replace(' ', '_').replace('-', '_')
-__version__ = package['version']
+package_name = package["name"].replace(" ", "_").replace("-", "_")
+__version__ = package["version"]
 
 _current_path = _os.path.dirname(_os.path.abspath(__file__))
 
@@ -31,10 +34,9 @@ _this_module = _sys.modules[__name__]
 
 _js_dist = [
     {
-        'relative_package_path': 'crystal_toolkit.min.js',
-        'dev_package_path': 'crystal_toolkit.dev.js',
-        
-        'namespace': package_name
+        "relative_package_path": "crystal_toolkit.min.js",
+        "dev_package_path": "crystal_toolkit.dev.js",
+        "namespace": package_name,
     }
 ]
 
@@ -42,9 +44,13 @@ _css_dist = []
 
 
 for _component in __all__:
-    setattr(locals()[_component], '_js_dist', _js_dist)
-    setattr(locals()[_component], '_css_dist', _css_dist)
+    setattr(locals()[_component], "_js_dist", _js_dist)
+    setattr(locals()[_component], "_css_dist", _css_dist)
 
 # convenience imports
 # TODO: rename view to dash_view, add pythreejs_view etc.
-from crystal_toolkit.helpers.view import view
+try:
+    import pythreejs
+    from crystal_toolkit.helpers.pythreejs_renderer import view as display
+except ImportError:
+    pass
