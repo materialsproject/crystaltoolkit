5861684ae62f14454928c44e840d24a52e1ff8c0
Shyam Dwaraknath <shyamd@lbl.gov>
2019-07-30 11:33:51 -0400

    register new renderers

--- crystal_toolkit/renderables/__init__.py
+++ crystal_toolkit/renderables/__init__.py
@@ -1,15 +1,19 @@
 from crystal_toolkit.core.renderable import Renderer
-from crystal_toolkit.renderables.lattice import get_lattice_scene
-from crystal_toolkit.renderables.site import SiteRenderable
+from crystal_toolkit.renderables.lattice import LatticeRenderer
+from crystal_toolkit.renderables.site import DefaultSiteRenderer
 from crystal_toolkit.renderables.moleculegraph import get_molecule_graph_scene
 from crystal_toolkit.renderables.structuregraph import get_structure_graph_scene
 
 from pymatgen import Lattice, Site
+from pymatgen.core.structure import SiteCollection
 from pymatgen.analysis.graphs import MoleculeGraph
 from pymatgen.analysis.graphs import StructureGraph
 
 
-Renderer.register_interface(Lattice,get_lattice_scene)
-Renderer.register_interface(Site,SiteRenderable.get_site_scene)
+lattice_renderer = LatticeRenderer()
+site_renderer = DefaultSiteRenderer()
+
+Renderer.register_interface(Lattice,lattice_renderer.to_scene)
+Renderer.register_interface(Site,site_renderer.to_scene)
 Renderer.register_interface(MoleculeGraph,get_molecule_graph_scene)
 Renderer.register_interface(StructureGraph,get_structure_graph_scene)
