90f1de0ec7d4c17924510f4c1d7ddac420181e05
JSX <jmmshn@gmail.com>
2019-08-07 14:27:23 -0700

    update
    update

--- crystal_toolkit/components/structure.py
+++ crystal_toolkit/components/structure.py
@@ -868,10 +868,7 @@ class StructureMoleculeComponent(MPComponent):
         legend = {"composition": struct_or_mol.composition.as_dict(), "colors": {}}
 
         # don't calculate color if one is explicitly supplied
-        if "display_color" in struct_or_mol.site_properties and struct_or_mol.site_properties[
-                "display_color"] is not None and all(
-                    isinstance(x, float)
-                    for x in struct_or_mol.site_properties["display_color"]):
+        if "display_color" in struct_or_mol.site_properties:
             # don't know what the color legend (meaning) is, so return empty legend
             return (struct_or_mol.site_properties["display_color"], legend)
 
@@ -1052,10 +1049,7 @@ class StructureMoleculeComponent(MPComponent):
         """
 
         # don't calculate radius if one is explicitly supplied
-        if "display_radius" in struct_or_mol.site_properties and struct_or_mol.site_properties[
-                "display_radius"] is not None and all(
-                    isinstance(x, float)
-                    for x in struct_or_mol.site_properties["display_radius"]):
+        if "display_radius" in struct_or_mol.site_properties:
             return struct_or_mol.site_properties["display_radius"]
 
         if (
