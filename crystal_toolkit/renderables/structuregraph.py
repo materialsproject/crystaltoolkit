c939ea1084de4c9e12caa8e4c236e261da39150f
Shyam Dwaraknath <shyamd@lbl.gov>
2019-08-20 08:28:10 -0700

    new structure graph renderer

--- crystal_toolkit/renderables/structuregraph.py
+++ crystal_toolkit/renderables/structuregraph.py
@@ -1,189 +1,193 @@
-from collections import defaultdict
+import numpy as np
 from itertools import combinations
 
-import numpy as np
 from pymatgen import PeriodicSite
+from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
 
 from crystal_toolkit.core.scene import Scene
-
-from matplotlib.cm import get_cmap
-
-from crystal_toolkit.renderables import get_site_scene, get_lattice_scene
-
-def _get_sites_to_draw(
-    structure_graph, draw_image_atoms=True, bonded_sites_outside_unit_cell=False
-):
-    """
-    Returns a list of site indices and image vectors.
-    """
-
-    sites_to_draw = [(idx, (0, 0, 0)) for idx in range(len(structure_graph.structure))]
-
-    if draw_image_atoms:
-
-        for idx, site in enumerate(structure_graph.structure):
-
-            zero_elements = [
-                idx
-                for idx, f in enumerate(site.frac_coords)
-                if np.allclose(f, 0, atol=0.05)
-            ]
-
-            coord_permutations = [
-                x
-                for l in range(1, len(zero_elements) + 1)
-                for x in combinations(zero_elements, l)
-            ]
-
-            for perm in coord_permutations:
-                sites_to_draw.append(
-                    (idx, (int(0 in perm), int(1 in perm), int(2 in perm)))
-                )
-
-            one_elements = [
-                idx
-                for idx, f in enumerate(site.frac_coords)
-                if np.allclose(f, 1, atol=0.05)
-            ]
-
-            coord_permutations = [
-                x
-                for l in range(1, len(one_elements) + 1)
-                for x in combinations(one_elements, l)
-            ]
-
-            for perm in coord_permutations:
-                sites_to_draw.append(
-                    (idx, (-int(0 in perm), -int(1 in perm), -int(2 in perm)))
-                )
-
-    if bonded_sites_outside_unit_cell:
-
-        sites_to_append = []
-        for (n, jimage) in sites_to_draw:
-            connected_sites = structure_graph.get_connected_sites(n, jimage=jimage)
-            for connected_site in connected_sites:
-                if connected_site.jimage != (0, 0, 0):
-                    sites_to_append.append(
-                        (connected_site.index, connected_site.jimage)
-                    )
-        sites_to_draw += sites_to_append
-
-    # remove any duplicate sites
-    # (can happen when enabling bonded_sites_outside_unit_cell,
-    #  since this works by following bonds, and a single site outside the
-    #  unit cell can be bonded to multiple atoms within it)
-    return set(sites_to_draw)
+from crystal_toolkit.renderables.site import StructureRenderer
 
 
-def get_structure_graph_scene(
-    structure_graph,
-    origin=(0, 0, 0),
-    draw_image_atoms=True,
-    bonded_sites_outside_unit_cell=True,
-    hide_incomplete_edges=False,
-    incomplete_edge_length_scale=0.3,
-    color_edges_by_edge_weight=True,
-    edge_weight_color_scale="coolwarm",
-    explicitly_calculate_polyhedra_hull=False,
-) -> Scene:
+class StructureGraphRenderer(StructureRenderer):
+    def __init__(
+        self,
+        draw_bonds=True,
+        draw_polyhedra=True,
+        draw_connected_atoms=True,
+        color_edges_by_edge_weight=True,
+        edge_weight_color_scale="coolwarm",
+        explicitly_calculate_polyhedra_hull=False,
+        **kwargs
+    ):
 
-    primitives = defaultdict(list)
+        self.draw_bonds = draw_bonds
+        self.draw_polyhedra = draw_polyhedra
+        self.draw_connected_atoms = draw_connected_atoms
+        self.color_edges_by_edge_weight = color_edges_by_edge_weight
+        self.edge_weight_color_scale = edge_weight_color_scale
+        self.explicitly_calculate_polyhedra_hull = explicitly_calculate_polyhedra_hull
 
-    sites_to_draw = _get_sites_to_draw(
-        structure_graph=structure_graph,
-        draw_image_atoms=draw_image_atoms,
-        bonded_sites_outside_unit_cell=bonded_sites_outside_unit_cell,
-    )
+        super().__init__(**kwargs)
 
-    color_edges = False
-    if color_edges_by_edge_weight:
+    def to_scene(self, structure_graph, origin=(0, 0, 0)):
 
-        weights = [e[2].get("weight") for e in structure_graph.graph.edges(data=True)]
-        weights = np.array([w for w in weights if w])
+        scenes = []
+        scenes.extend(super().to_scene(structure_graph.structure, origin).contents)
 
-        if any(weights):
-
-            cmap = get_cmap(edge_weight_color_scale)
-
-            # try to keep color scheme symmetric around 0
-            weight_max = max([abs(min(weights)), max(weights)])
-            weight_min = -weight_max
-
-            def get_weight_color(weight):
-                if not weight:
-                    weight = 0
-                x = (weight - weight_min) / (weight_max - weight_min)
-                return "#{:02x}{:02x}{:02x}".format(
-                    *[int(c * 255) for c in cmap(x)[0:3]]
-                )
-
-            color_edges = True
-
-    for (idx, jimage) in sites_to_draw:
-
-        site = structure_graph.structure[idx]
-        if jimage != (0, 0, 0):
-            connected_sites = structure_graph.get_connected_sites(idx, jimage=jimage)
-            site = PeriodicSite(
-                site.species,
-                np.add(site.frac_coords, jimage),
-                site.lattice,
-                properties=site.properties,
-            )
-        else:
-            connected_sites = structure_graph.get_connected_sites(idx)
-
-        connected_sites = [
-            cs for cs in connected_sites if (cs.index, cs.jimage) in sites_to_draw
-        ]
-        connected_sites_not_drawn = [
-            cs for cs in connected_sites if (cs.index, cs.jimage) not in sites_to_draw
-        ]
-
-        if color_edges:
-
-            connected_sites_colors = [
-                get_weight_color(cs.weight) for cs in connected_sites
-            ]
-            connected_sites_not_drawn_colors = [
-                get_weight_color(cs.weight) for cs in connected_sites_not_drawn
-            ]
-
-        else:
-
-            connected_sites_colors = None
-            connected_sites_not_drawn_colors = None
-
-        site_scene = get_site_scene(
-            site=site,
-            connected_sites=connected_sites,
-            connected_sites_not_drawn=connected_sites_not_drawn,
-            hide_incomplete_edges=hide_incomplete_edges,
-            incomplete_edge_length_scale=incomplete_edge_length_scale,
-            connected_sites_colors=connected_sites_colors,
-            connected_sites_not_drawn_colors=connected_sites_not_drawn_colors,
-            origin=origin,
-            explicitly_calculate_polyhedra_hull=explicitly_calculate_polyhedra_hull,
+        boundary_images = (
+            set(self.find_boundary_images(structure))
+            if self.draw_boundary_images
+            else {}
         )
-        for scene in site_scene.contents:
-            primitives[scene.name] += scene.contents
 
-    # we are here ...
-    # select polyhedra
-    # split by atom type at center
-    # see if any intersect, if yes split further
-    # order sets, with each choice, go to add second set etc if don't intersect
-    # they intersect if centre atom forms vertex of another atom (caveat: centre atom may not actually be inside polyhedra! not checking for this, add todo)
-    # def _set_intersects() ->bool:
-    # def _split_set() ->List: (by type, then..?)
-    # def _order_sets()... pick 1, ask can add 2? etc
+        images = (
+            self.find_connected_atoms(structure_graph, boundary_images)
+            if self.draw_connected_atoms
+            else {}
+        )
+        base_atoms = [(idx, (0, 0, 0)) for idx in range(len(structure_graph.structure))]
 
-    primitives["unit_cell"].append(
-        get_lattice_scene(lattice=structure_graph.structure.lattice,origin=origin)
-    )
+        # All drawn atoms
+        all_drawn_atoms = set(base_atoms) + set(boundary_images) + set(images)
 
-    return Scene(
-        name=structure_graph.structure.composition.reduced_formula,
-        contents=[Scene(name=k, contents=v) for k, v in primitives.items()],
-    )
+        if self.draw_connected_atoms:
+
+            image_atoms = [
+                PeriodicSite(
+                    structure_graph.structure[idx].species,
+                    np.add(structure_graph.structure[idx].frac_coords, jimage),
+                    structure_graph.structure[idx].lattice,
+                    properties=structure_graph.structure[idx].properties,
+                )
+                for idx, jimage in images
+            ]
+
+            image_atoms_scene = Scene(
+                name="image_atoms",
+                contents=list(
+                    chain.from_iterable(
+                        [
+                            self.site_renderer.to_scene(s, origin).contents
+                            for s in image_atoms
+                        ]
+                    )
+                ),
+            )
+            scenes.append(image_atoms_scene)
+
+        if self.draw_bonds:
+            bonds = []
+            image_bonds = []
+
+            # Set of all atoms we have drawn
+            all_drawn_atoms = set(base_atoms) + set(boundary_images) + set(images)
+
+            for idx, jimage in all_drawn_atoms:
+
+                site = structure_graph.structure[idx]
+                position = site.coords
+
+                for connected_site in structure_graph.get_connected_sites(
+                    idx, jimage=jimage
+                ):
+
+                    bond_midpoint = np.add(position, connected_position) / 2
+
+                    if len(site.species) > 1:
+                        color = "#555555"
+                    else:
+                        color = connected_site.properties.get("display_color", [None])[
+                            0
+                        ] or self.site_renderer.color(list(site.species.keys())[0])
+
+                    connected_position = np.subtract(connected_site.site.coords, origin)
+
+                    bond_cylinder = Cylinders(
+                        positionPairs=[[position, bond_midpoint.tolist()]], color=color
+                    )
+                    # If the connected site is also being drawn
+                    if (connected_site.index, connected_site.jimage) in all_drawn_atoms:
+                        bonds.append(bond_cylinder)
+                    else:
+                        image_bonds.append(bond_cylinder)
+
+            scenes.append(Scene(name="bonds", contents=bonds))
+            scenes.append(Scene(name="image_bonds", contents=image_bonds))
+
+        if self.draw_polyhedra:
+
+            polyhedra = []
+            # Only draw polyhedra for atoms where we include all connected sites
+            for idx, jimage in all_drawn_atoms:
+
+                site = structure_graph.structure[idx]
+                position = site.coords
+                connected_sites = structure_graph.get_connected_sites(
+                    idx, jimage=jimage
+                )
+                # Make a polyhedra if all connected sites are visible
+                if all(c in all_drawn_atoms for c in connected_sites):
+                    connected_positions = [
+                        np.subtract(connected_site.site.coords, origin).tolist()
+                        for connected_site in connected_sites
+                    ]
+                    try:
+
+                        # connected_positions = [[0, 0, 0], [0, 0, 10], [0, 10, 0], [10, 0, 0]]
+                        # gives...
+                        # .convex_hull = [[2, 3, 0], [1, 3, 0], [1, 2, 0], [1, 2, 3]]
+                        # .vertex_neighbor_vertices = [1, 2, 3, 2, 3, 0, 1, 3, 0, 1, 2, 0]
+
+                        vertices_indices = Delaunay(
+                            connected_positions
+                        ).vertex_neighbor_vertices
+                        vertices = [
+                            connected_positions[idx] for idx in vertices_indices
+                        ]
+
+                        if len(site.species) > 1:
+                            color = "#555555"
+                        else:
+                            color = connected_site.properties.get(
+                                "display_color", [None]
+                            )[0] or self.site_renderer.color(
+                                list(site.species.keys())[0]
+                            )
+
+                        polyhedra.append(Surface(positions=vertices, color=color))
+
+                    except Exception as e:
+                        pass
+                scenes.append(Scene(name="polyhedra", contents=polyhedra))
+
+        return Scene(contents=scenes)
+
+    def find_connected_atoms(self, structure_graph, images=None):
+        """
+        Finds all connected atoms one connection out from the structure.
+        Will work out one connection out from any given image atoms as well
+        """
+
+        images = images or []
+
+        base_connections = [
+            (connected_site.index, connected_site.jimage)
+            for n, _ in enumerate(structure_graph.structure)
+            for connected_site in structure_graph.get_connected_sites(
+                n, jimage=(0, 0, 0)
+            )
+        ]
+
+        image_connections = [
+            (connected_site.index, connected_site.jimage)
+            for n, jimage in images
+            for connected_site in structure_graph.get_connected_sites(n, jimage)
+        ]
+
+        sites_to_append = {
+            (n, jimage)
+            for n, jimage in chain(base_connections, image_connections)
+            if jimage != (0, 0, 0)
+        }
+
+        return sites_to_append - images
