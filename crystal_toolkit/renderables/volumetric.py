2efb5f49f24ee8c4c5f5f3b250d184c011d66377
Matthew Horton <mkhorton@users.noreply.github.com>
2019-08-22 11:27:59 -0700

    Remove print statement

--- crystal_toolkit/renderables/volumetric.py
+++ crystal_toolkit/renderables/volumetric.py
@@ -13,12 +13,11 @@ def get_volumetric_scene(self,
                          step_size=3,
                          **kwargs):
     o = -np.array(origin)
-    print(self.data)
     vertices, faces, normals, values = measure.marching_cubes_lewiner(
         self.data[data_key], level=isolvl, step_size=step_size)
     vertices = vertices/self.data[data_key].shape  # transform to fractional coordinates
     vertices = np.dot(o + vertices, self.structure.lattice.matrix) # transform to cartesian
     return Scene("volumetric-data", contents=[Surface(vertices, normals, **kwargs)])
 
-# todo: re-think origin, shift globally at end (scene.origin)
+# TODO: re-think origin, shift globally at end (scene.origin)
 VolumetricData.get_scene = get_volumetric_scene
