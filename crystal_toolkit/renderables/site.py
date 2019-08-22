1bd6d140765929eaded7e955848eca8a94e277f4
JSX <jmmshn@gmail.com>
2019-08-22 11:07:16 -0700

    display surfaces in pythreejs - updated sites.py so that it only draws polygon for cations - use the Delaunay().convexhull for explicitly_claculate_polyhedra_hull=True

--- crystal_toolkit/renderables/site.py
+++ crystal_toolkit/renderables/site.py
@@ -3,7 +3,7 @@ from pymatgen import DummySpecie
 from scipy.spatial.qhull import Delaunay
 
 from crystal_toolkit.core.scene import Scene, Cubes, Spheres, Cylinders, Surface, Convex
-
+from itertools import chain
 from pymatgen import Site
 from pymatgen.analysis.graphs import ConnectedSite
 
@@ -164,11 +164,12 @@ def get_site_scene(
                 )
                 bonds.append(cylinder)
                 all_positions.append(connected_position.tolist())
-
+        not_most_electro_neg = map(lambda x : x.site.specie < self.specie, connected_sites)
         if (
             draw_polyhedra
             and len(connected_sites) > 3
             and not connected_sites_not_drawn
+            and not any(not_most_electro_neg)
         ):
             if explicitly_calculate_polyhedra_hull:
 
@@ -179,19 +180,18 @@ def get_site_scene(
                     # .convex_hull = [[2, 3, 0], [1, 3, 0], [1, 2, 0], [1, 2, 3]]
                     # .vertex_neighbor_vertices = [1, 2, 3, 2, 3, 0, 1, 3, 0, 1, 2, 0]
 
-                    vertices_indices = Delaunay(all_positions).vertex_neighbor_vertices
-                    vertices = [all_positions[idx] for idx in vertices_indices]
-
-                    polyhedron = [
-                        Surface(
-                            positions=vertices,
-                            color=self.properties["display_color"][0],
-                        )
-                    ]
-
+                    vertices_indices = Delaunay(all_positions).convex_hull
                 except Exception as e:
+                    vertices_indices=[]
 
-                    polyhedron = []
+                vertices = [all_positions[idx] for idx in chain.from_iterable(vertices_indices)]
+
+                polyhedron = [
+                    Surface(
+                        positions=vertices,
+                        color=self.properties["display_color"][0],
+                    )
+                ]
 
             else:
 
