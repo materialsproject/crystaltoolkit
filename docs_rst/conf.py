336faa9f474f38ee5d5dfae2e80e89897d6eabee
Matthew Horton <mkhorton@lbl.gov>
2020-01-02 21:38:16 -0800

    Add Napoleon

--- docs_rst/conf.py
+++ docs_rst/conf.py
@@ -45,6 +45,7 @@ extensions = [
     "sphinx.ext.viewcode",
     "sphinx.ext.githubpages",
     "sphinx.ext.autosectionlabel",
+    "sphinx.ext.napoleon",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
