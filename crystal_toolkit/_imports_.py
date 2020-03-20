bb07f4a0dc2a7c9a7d873427b4384e48891bc214
Matthew Horton <mkhorton@lbl.gov>
2020-03-20 13:27:59 -0700

    Remove JS

--- crystal_toolkit/_imports_.py
+++
@@ -1,9 +0,0 @@
-from .GraphComponent import GraphComponent
-from .JSONViewComponent import JSONViewComponent
-from .Simple3DSceneComponent import Simple3DSceneComponent
-
-__all__ = [
-    "GraphComponent",
-    "JSONViewComponent",
-    "Simple3DSceneComponent"
-]
--- setup.py
+++ setup.py
@@ -3,12 +3,6 @@ import os
 from setuptools import setup, find_packages
 
 
-with open(os.path.join("crystal_toolkit", "package.json")) as f:
-    package = json.load(f)
-
-package_name = package["name"].replace(" ", "_").replace("-", "_")
-
-
 def requirements(fname):
     with open(fname, "r") as f:
         reqs = f.read().splitlines()
@@ -22,14 +16,14 @@ def requirements(fname):
 
 
 setup(
-    name=package_name,
-    version=package["version"],
-    author=package["author"],
+    name="crystal_toolkit",
+    # version=package["version"],
+    # author=package["author"],
     packages=find_packages(),
     package_data={"": ["*.json", "*.js", "*.yaml"]},
     include_package_data=True,
-    license=package["license"],
-    description=package["description"] if "description" in package else package_name,
+    # license=package["license"],
+    # description=package["description"] if "description" in package else package_name,
     install_requires=requirements("requirements.txt"),
     python_requires=">=3.7",
 )
