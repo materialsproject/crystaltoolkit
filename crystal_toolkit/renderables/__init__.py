71effa81256c32704b5f5a9bd41f80bf7b9698d6
Matthew Horton <mkhorton@lbl.gov>
2020-05-13 15:59:54 -0700

    Add JupyterLab support

--- crystal_toolkit/renderables/__init__.py
+++ crystal_toolkit/renderables/__init__.py
@@ -5,3 +5,25 @@ from crystal_toolkit.renderables.structuregraph import StructureGraph
 from crystal_toolkit.renderables.structure import Structure
 from crystal_toolkit.renderables.volumetric import VolumetricData
 from crystal_toolkit.renderables.sitecollection import PeriodicSite
+
+
+def _repr_mimebundle_(self, include=None, exclude=None):
+    """
+    Render Scenes using crystaltoolkit-extension for Jupyter Lab.
+    """
+    return {
+        "application/vnd.mp.v1+json": self.get_scene().to_json(),
+        "text/plain": self.__repr__(),
+    }
+
+
+for cls in (
+    Lattice,
+    MoleculeGraph,
+    Site,
+    StructureGraph,
+    Structure,
+    VolumetricData,
+    PeriodicSite,
+):
+    cls._repr_mimebundle_ = _repr_mimebundle_
