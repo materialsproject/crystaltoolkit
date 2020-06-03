acf2063b07c5980722bab9ba29b092b0f85f59f9
Matthew Horton <mkhorton@users.noreply.github.com>
2020-06-03 01:52:39 +0000

    Set version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ readme = ""
 setup(
     long_description=readme,
     name="crystal_toolkit",
-    version="2020.05.21",
+    version="2020.06.03",
     python_requires="==3.*,>=3.7.0",
     author="Matthew Horton",
     author_email="mkhorton@lbl.gov",
