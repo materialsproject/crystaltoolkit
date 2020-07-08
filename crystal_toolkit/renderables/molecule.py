7725d0e1755191a0f39018b2285772ed90dfc69c
jmmshn <jmmshn@gmail.com>
2020-07-08 12:59:10 -0700

    lint

--- crystal_toolkit/renderables/molecule.py
+++ crystal_toolkit/renderables/molecule.py
@@ -1,15 +1,23 @@
 from collections import defaultdict
-from crystal_toolkit.components.structure import StructureMoleculeComponent as SMC
-from pymatgen import Molecule
 from typing import Optional
-import crystal_toolkit.renderables.structuregraph
+
+from pymatgen import Molecule
+
 from crystal_toolkit.core.legend import Legend
 from crystal_toolkit.core.scene import Scene
 
 
-def get_scene_from_molecule(self,
-                            origin=None,
-                            legend: Optional[Legend] = None):
+def get_scene_from_molecule(self, origin=None, legend: Optional[Legend] = None):
+    """
+    Create CTK objects for the lattice and sties
+    Args:
+        self:  Structure object
+        origin: fractional coordinate of the origin
+        legend: Legend for the sites
+
+    Returns:
+        CTK scene object to be rendered
+    """
 
     origin = origin if origin else (0, 0, 0)
 
@@ -18,10 +26,7 @@ def get_scene_from_molecule(self,
     primitives = defaultdict(list)
 
     for idx, site in enumerate(self):
-        site_scene = site.get_scene(
-            origin=origin,
-            legend=legend,
-        )
+        site_scene = site.get_scene(origin=origin, legend=legend,)
 
         for scene in site_scene.contents:
             primitives[scene.name] += scene.contents
