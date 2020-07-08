5818a41c16d41afe3684a413c19727c8c3d4035a
jmmshn <jmmshn@lbl.gov>
2020-07-08 12:33:16 -0700

    fixes for structure and molecule get_scene

--- crystal_toolkit/renderables/__init__.py
+++ crystal_toolkit/renderables/__init__.py
@@ -1,5 +1,6 @@
 from crystal_toolkit.renderables.lattice import Lattice
 from crystal_toolkit.renderables.moleculegraph import MoleculeGraph
+from crystal_toolkit.renderables.molecule import Molecule
 from crystal_toolkit.renderables.site import Site
 from crystal_toolkit.renderables.structuregraph import StructureGraph
 from crystal_toolkit.renderables.structure import Structure
--- crystal_toolkit/renderables/molecule.py
+++ crystal_toolkit/renderables/molecule.py
@@ -1,18 +1,36 @@
+from collections import defaultdict
 from crystal_toolkit.components.structure import StructureMoleculeComponent as SMC
 from pymatgen import Molecule
+from typing import Optional
 import crystal_toolkit.renderables.structuregraph
+from crystal_toolkit.core.legend import Legend
+from crystal_toolkit.core.scene import Scene
 
 
 def get_scene_from_molecule(self,
-                             bonding_strategy="CrystalNN",
-                             bonding_strategy_kwargs=None,
-                             **kwargs):
-    sgraph = SMC._preprocess_input_to_graph(self,
-                                            bonding_strategy=bonding_strategy,
-                                            bonding_strategy_kwargs=bonding_strategy_kwargs,
-                                            )
-    return sgraph.get_scene(origin=None,
-                            **kwargs)
+                            origin=None,
+                            legend: Optional[Legend] = None):
+
+    origin = origin if origin else (0, 0, 0)
+
+    legend = legend or Legend(self)
+
+    primitives = defaultdict(list)
+
+    for idx, site in enumerate(self):
+        site_scene = site.get_scene(
+            origin=origin,
+            legend=legend,
+        )
+
+        for scene in site_scene.contents:
+            primitives[scene.name] += scene.contents
+
+    return Scene(
+        name=self.composition.reduced_formula,
+        contents=[Scene(name=k, contents=v) for k, v in primitives.items()],
+        origin=origin,
+    )
 
 
 Molecule.get_scene = get_scene_from_molecule
