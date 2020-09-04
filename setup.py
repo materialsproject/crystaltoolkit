391dea4b446c434c9091912a60b08b4ef331e81c
Matthew Horton <m.k.horton@gmail.com>
2020-09-03 17:49:47 -0700

    Remove setup.py, dependencies in favor of poetry

--- setup.py
+++ setup.py
@@ -37,30 +37,6 @@ setup(
         "crystal_toolkit.apps.examples": ["*.json"],
         "crystal_toolkit.core": ["*.yaml"],
     },
-    install_requires=[
-        "dash[testing]==1.*,>=1.9.1",
-        "dash-daq==0.*,>=0.4.0",
-        "dash-mp-components==0.*,>=0.0.8",
-        "dash-table==4.*,>=4.6.1",
-        "flask-caching==1.*,>=1.8.0",
-        "gevent==1.*,>=1.4.0",
-        "gunicorn==20.*,>=20.0.4",
-        "plotly==4.*,>=4.5.4",
-        "pydantic==1.*,>=1.4.0",
-        "pymatgen==2020.*,>=2020.3.13",
-        "redis==3.*,>=3.4.1",
-        "scikit-learn==0.*,>=0.22.2",
-        "webcolors==1.*,>=1.11.1",
-    ],
-    extras_require={
-        "dev": [
-            "black==19.*,>=19.10.0",
-            "dephell==0.*,>=0.8.2",
-            "pre-commit==2.*,>=2.2.0",
-            "pytest==5.*,>=5.4.1",
-            "recommonmark==0.*,>=0.6.0",
-            "sphinx==2.*,>=2.4.4",
-            "sphinx-rtd-theme==0.*,>=0.4.3",
-        ]
-    },
+    install_requires=[],
+    extras_require={"dev": []},
 )
