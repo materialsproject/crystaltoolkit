1bf52e64ce2e9bf4f79a93fd4133690ff0fd6766
Matthew Horton <m.k.horton@gmail.com>
2020-09-26 16:34:30 -0700

    Add PhaseDiagram plotting, MSONable.show_json()

--- crystal_toolkit/__init__.py
+++ crystal_toolkit/__init__.py
@@ -36,23 +36,41 @@ def _repr_mimebundle_(self, include=None, exclude=None):
     """
     # TODO: add Plotly, application/vnd.plotly.v1+json
 
-    help_text = """If you see this text, the Crystal Toolkit Jupyter Lab \n
+    help_text_ct = """If you see this text, the Crystal Toolkit Jupyter Lab \n
 extension is not installed. You can install it by running \n
 \"jupyter labextension install crystaltoolkit-extension\" \n
 from the same environment you run \"jupyter lab"\. \n\n
 """
 
+    help_text_plotly = """If you see this text, the Plotly Jupyter Lab extension 
+is not installed, please consult Plotly documentation for information on how to 
+install.
+"""
+
     if hasattr(self, "get_scene"):
         return {
             "application/vnd.mp.ctk+json": self.get_scene().to_json(),
-            # "application/json": self.as_dict(),
-            "text/plain": help_text + self.__repr__(),
+            "text/plain": help_text_ct + self.__repr__(),
+        }
+    elif hasattr(self, "get_plot"):
+        return {
+            "application/vnd.plotly.v1+json": self.get_plot().to_plotly_json(),
+            "text/plain": help_text_plotly + self.__repr__(),
         }
     else:
         return {"application/json": self.as_dict(), "text/plain": self.__repr__()}
 
 
 MSONable._repr_mimebundle_ = _repr_mimebundle_
+
+
+def show_json(self):
+    from IPython.display import display_json
+
+    return display_json(self.as_dict(), raw=True)
+
+
+MSONable.show_json = show_json
 
 
 def _ipython_display_(self):
