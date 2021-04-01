99e50d2622fbdea9438acf486ba143ea287119d6
Matthew Horton <mkhorton@users.noreply.github.com>
2021-03-31 22:54:13 -0700

    Update version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ readme = ""
 setup(
     long_description=readme,
     name="crystal_toolkit",
-    version="2021.03.25",
+    version="2021.03.31",
     python_requires="==3.*,>=3.7.0",
     author="Matthew Horton",
     author_email="mkhorton@lbl.gov",
