6aa1eaa2e871e4dc558d942b3b372190178135dc
Matthew Horton <mkhorton@users.noreply.github.com>
2021-10-11 19:06:58 -0700

    Update for new MP API

--- crystal_toolkit/components/submit_snl.py
+++ crystal_toolkit/components/submit_snl.py
@@ -3,9 +3,9 @@ from urllib import parse
 import requests
 from dash.dependencies import Input, Output, State
 from dash.exceptions import PreventUpdate
-from pymatgen.ext.matproj import MPRester
 from pymatgen.core.structure import Structure
 from pymatgen.util.provenance import StructureNL
+from mp_api import MPRester
 
 from crystal_toolkit import __version__ as ct_version
 from crystal_toolkit.core.panelcomponent import PanelComponent
