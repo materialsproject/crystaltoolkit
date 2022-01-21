aa68c1dc20486944c8c2ea06a998b576df8fba79
Cody O'Donnell <ctodonnell@lbl.gov>
2022-01-21 14:02:36 -0800

    Increase version

--- crystal_toolkit/__init__.py
+++ crystal_toolkit/__init__.py
@@ -8,7 +8,7 @@ from monty.json import MSONable
 
 from crystal_toolkit.renderables import *
 
-__version__ = "2021.12.22.01"
+__version__ = "2022.01.21"
 
 MODULE_PATH = Path(__file__).parents[0]
 
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ readme = ""
 setup(
     long_description=readme,
     name="crystal_toolkit",
-    version="2021.12.22.01",
+    version="2022.01.21",
     python_requires="==3.*,>=3.8.0",
     author="Matthew Horton",
     author_email="mkhorton@lbl.gov",
