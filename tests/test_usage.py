0b8910a1fb9ea5d3cd19a0fe7019d8e952ce033c
Janosh Riebesell <janosh.riebesell@gmail.com>
2022-07-27 11:45:21 -0700

    update tests/test_usage.py to use dash.testing, not pytest-dash

--- tests/test_usage.py
+++ tests/test_usage.py
@@ -1,31 +1,14 @@
-from pytest_dash.utils import (
-    import_app,
-    wait_for_element_by_css_selector,
-    wait_for_text_to_equal,
-)
+from dash.testing.application_runners import import_app
+
+from .conftest import DashDuo
 
 
-# Basic test for the component rendering.
-def test_render_component(dash_threaded, selenium):
-    # Start a dash app contained in `usage.py`
-    # dash_threaded is a fixture by pytest-dash
+def test_example_app(dash_duo: DashDuo):
+    # dash_duo is a fixture by dash.testing.plugin.dash_duo.
     # It will load a py file containing a Dash instance named `app`
     # and start it in a thread.
-    app = import_app("usage")
-    dash_threaded(app)
+    app = import_app("crystal_toolkit.apps.examples.basic_hello_world")
+    dash_duo.start_server(app)
+    wrapper_div = dash_duo.find_element("#_dash-app-content")
 
-    # Get the generated component input with selenium
-    # The html input will be a children of the #input dash component
-    my_component = wait_for_element_by_css_selector(selenium, "#input > input")
-
-    assert "my-value" == my_component.get_attribute("value")
-
-    # Clear the input
-    my_component.clear()
-
-    # Send keys to the custom input.
-    my_component.send_keys("Hello dash")
-
-    # Wait for the text to equal, if after the timeout (default 10 seconds)
-    # the text is not equal it will fail the test.
-    wait_for_text_to_equal(selenium, "#output", "You have entered Hello dash")
+    assert wrapper_div.text == "_dash-app-content"
