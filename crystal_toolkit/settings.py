ada2ca541f78d62ef3ec166e92cec38ec3a98506
Brook Wander <brook.l.wander@gmail.com>
2022-09-07 23:07:12 -0400

    adding optional (typing)

--- crystal_toolkit/settings.py
+++ crystal_toolkit/settings.py
@@ -1,4 +1,4 @@
-from typing import Literal
+from typing import Literal, Optional
 
 from pydantic import BaseSettings
 
@@ -17,7 +17,7 @@ class Settings(BaseSettings):
 
     DEV_LOGIN_DISABLED: bool = True
     LOGIN_ENDPOINT: str = "https://profile.materialsproject.org/"
-    API_KEY = "IVGSPgUkVx6PXqBl2xr4N53jE3y8xoUG"
+    API_KEY: Optional[str] = ""
     API_EXTERNAL_ENDPOINT: str = "https://api.materialsproject.org"
 
     PERSISTENCE: bool = True
