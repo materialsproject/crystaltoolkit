e249c9784ebce0e408b1cede087db919ac1ecaeb
SE Zeltmann <steven.zeltmann@lbl.gov>
2022-10-20 14:36:47 -0700

    build out options in example app

--- crystal_toolkit/components/diffraction_tem.py
+++ crystal_toolkit/components/diffraction_tem.py
@@ -12,7 +12,7 @@ from scipy.special import wofz
 import py4DSTEM
 
 from crystal_toolkit.core.mpcomponent import MPComponent
-from crystal_toolkit.helpers.layouts import Box, Column, Columns, Loading
+from crystal_toolkit.helpers.layouts import Box, Column, Columns, Loading, Reveal
 
 # Author: Steven Zeltmann
 # Contact: steven.zeltmann@lbl.gov
@@ -22,14 +22,16 @@ class TEMDiffractionComponent(MPComponent):
     def __init__(self, *args, initial_structure=None, **kwargs):
         super().__init__(*args, **kwargs)
         self.create_store("structure", initial_data=initial_structure)
+        self.calculator = TEMDiffractionCalculator()
 
     def layout(self) -> Columns:
 
         voltage = self.get_numerical_input(
             kwarg_label="voltage",
             default=200,
+            step=50.,
             label="Voltage / kV",
-            help_str="Accelerating voltage for electron beam."
+            help_str="Accelerating voltage for electron beam.",
         )
 
         beam_direction = self.get_numerical_input(
@@ -41,13 +43,109 @@ class TEMDiffractionComponent(MPComponent):
             is_int=True,
         )
 
-        # TODO: add additional kwargs for TemCalculator, or switch to an alternative solution
+        k_max = self.get_numerical_input(
+            kwarg_label="k_max",
+            default=1.5,
+            step=0.25,
+            label="Maximum Scattering Angle [Å<sup>-1</sup>]",
+            help_str="Maximum scattering angle to compute reciprocal lattice.",
+        )
+
+        use_dynamical = self.get_bool_input(
+            kwarg_label="use_dynamical",
+            default=False,
+            label="Use Bloch Wave Dynamical Calculator",
+            help_str="The Bloch wave calculator gives accurate diffraction "
+            "intensities for thick crystals by including multiple scattering"
+            " of the electron beam, but requires substantially more computation time.",
+        )
+
+        thickness = self.get_numerical_input(
+            kwarg_label="thickness",
+            default=500.,
+            step=10.,
+            label="Thickness [Å]",
+            help_str="Sample thickness in Ångströms, for dynamical simulation.")
+
+        # Advanced options
+
+        excitation_tol = self.get_numerical_input(
+            kwarg_label="sigma_excitation_error",
+            default=0.02,
+            label="Excitation error tolerance [Å-1]",
+            help_str="Standard deviation of Gaussian function for damping")
+
+        Fhkl_tol = self.get_numerical_input(
+            kwarg_label="tol_intensity",
+            default=0.,
+            step=0.001,
+            label="|Fkhl| tolerance",
+            help_str="Minimum structure factor intensity to include a reflection.",
+        )
+
+        absorption_method_names = {
+                "Lobato (Elastic)":"Lobato",
+                "Lobato (Hashimoto absoprtive)": "Lobato-absorptive",
+                "Weickenmeier-Kohl (Elastic)": "WK",
+                "Weickenmeier-Kohl (Core only)": "WK-C",
+                "Weickenmeier-Kohl (Phonon only)": "WK-P",
+                "Weickenmeier-Kohl (Core + Phonon)": "WK-CP"
+            }
+
+        absorption_methods = self.get_choice_input(
+            kwarg_label="dynamical_method",
+            label="Scattering Factor Parameterization",
+            default="WK-CP",
+            help_str="Parameterization of absoprtive scattering factors, used only"
+                    " for dynamical calculations. Kinematic calculations always use Lobato",
+            options=[
+                {
+                    "label":name,
+                    "value":shortname,
+                }
+                for name, shortname in absorption_method_names.items()
+            ]
+        )
+
+        DWF = self.get_numerical_input(
+            kwarg_label="DWF",
+            label="RMS Atomic Displacements [Å]",
+            default=0.08,
+            step=0.01,
+            help_str="RMS atomic displacements used to include thermal smearing of"
+                    "the electrostatic potential when a Weickenmeier-Kohl scattering factor is chosen",
+        )
+
+        gamma = self.get_numerical_input(
+            kwarg_label="gamma",
+            label="Display Gamma",
+            default=1.0,
+            step=0.1,
+            help_str="Power for scaling intensities in the displayed pattern",
+        )
+
+        advanced_options = Reveal(
+            title="Advanced Options",
+            children=[excitation_tol, html.Br(), 
+                      Fhkl_tol, html.Br(),
+                      absorption_methods, html.Br(),
+                      DWF, html.Br(),
+                      gamma],
+            id="tem-advanced-options",
+        )
 
         return Columns(
             [
                 Column([Box(Loading(id=self.id("tem-plot")))], size=8),
                 Column(
-                    [voltage, html.Br(), beam_direction],
+                    [
+                        voltage, html.Br(), 
+                        beam_direction, html.Br(),
+                        k_max, html.Br(),
+                        use_dynamical, html.Br(),
+                        thickness, html.Br(),
+                        advanced_options,
+                    ],
                     size=4,
                 ),
             ],
@@ -66,37 +164,38 @@ class TEMDiffractionComponent(MPComponent):
             structure = self.from_data(structure)
             kwargs = self.reconstruct_kwargs_from_state()
 
-            calculator = TEMCalculator(**kwargs)
+            # calculator = TEMCalculator(**kwargs)
 
             print("kwargs", kwargs)
 
             return dcc.Graph(
-                figure=calculator.get_plot_2d(structure),
+                figure=self.calculator.get_plot_2d(structure,**kwargs),
                 responsive=False,
                 config=dict(displayModeBar=False, displaylogo=False),
             )
 
 
-class TEMDiffractionCalculator(AbstractDiffractionPatternCalculator):
+class TEMDiffractionCalculator:
     """
     Docstring
     """
 
-    def __init__(self)->None:
+    def __init__(self) -> None:
         self.crystal = None
 
     def get_plot_2d(
         self,
         structure,
         beam_direction,
-        voltage:float,
-        k_max:float,
-        thickness:float,
-        structure_factor_tol:float=None,
-        use_dynamical:bool=False,
+        voltage: float,
+        k_max: float,
+        thickness: float,
+        sigma_excitation_error: float = None,
+        use_dynamical: bool = False,
         dynamical_method=None,
-        DWF:float=None
-        **kwargs)->go:
+        DWF: float = None,
+        **kwargs,
+    ) -> go:
         """
         generate diffraction pattern and return as a plotly graph object
         """
@@ -104,44 +203,34 @@ class TEMDiffractionCalculator(AbstractDiffractionPatternCalculator):
         # check if cached structure factors are valid, recompute if needed
         # (and check if dynamical factors are needed)
         self.update_structure_factors(
-            structure,
-            k_max,
-            structure_factor_tol,
-            use_dynamical,
-            DWF)
+            structure, k_max, sigma_excitation_error, use_dynamical, DWF
+        )
 
         # generate diffraction pattern
         pattern = self.crystal.generate_diffraction_pattern(
-            zone_axis_lattice=beam_direction)
+            zone_axis_lattice=beam_direction
+        )
 
         # perform dynamical simulation, if Bloch is selected
         if use_dynamical:
             pattern = self.crystal.generate_dynamical_diffraction_pattern(
-                pattern,
-                thickness=thickness,
-                zone_axis_lattice=beam_direction)
+                pattern, thickness=thickness, zone_axis_lattice=beam_direction
+            )
 
         # generate plotly spots for each reflection
         spots = self.pointlist_to_spots(pattern)
 
         # wrap everything up into a figure
 
-
         pass
 
     def update_structure_factors(
-        self,
-        new_structure,
-        k_max,
-        structure_factor_tol,
-        use_dynamical,
-        DWF
-        ):
+        self, new_structure, k_max, sigma_excitation_error, use_dynamical, DWF
+    ):
         pass
 
     def pointlist_to_spots(
         self,
         pattern,
-        ):
+    ):
         pass
-
