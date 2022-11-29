c2b9de6ca8a27ce5ccbaaec5232b97593fe53b36
Matthew Horton <mkhorton@users.noreply.github.com>
2022-11-29 10:17:06 -0800

    `monty` should always be installed

--- crystal_toolkit/__init__.py
+++ crystal_toolkit/__init__.py
@@ -4,33 +4,27 @@ from pathlib import Path
 
 from pkg_resources import DistributionNotFound, get_distribution
 
-try:
-    from monty.json import MSONable
-except ImportError:
-    MSONable = None
+from crystal_toolkit.msonable import (
+    _ipython_display_,
+    _repr_mimebundle_,
+    show_json,
+    to_plotly_json,
+)
+from crystal_toolkit.renderables import (
+    Lattice,
+    Molecule,
+    MoleculeGraph,
+    PhaseDiagram,
+    Site,
+    Structure,
+    StructureGraph,
+    VolumetricData,
+)
 
-if MSONable:
-    from crystal_toolkit.msonable import (
-        _ipython_display_,
-        _repr_mimebundle_,
-        show_json,
-        to_plotly_json,
-    )
-    from crystal_toolkit.renderables import (
-        Lattice,
-        Molecule,
-        MoleculeGraph,
-        PhaseDiagram,
-        Site,
-        Structure,
-        StructureGraph,
-        VolumetricData,
-    )
-
-    MSONable.to_plotly_json = to_plotly_json
-    MSONable._repr_mimebundle_ = _repr_mimebundle_
-    MSONable.show_json = show_json
-    MSONable._ipython_display_ = _ipython_display_
+MSONable.to_plotly_json = to_plotly_json
+MSONable._repr_mimebundle_ = _repr_mimebundle_
+MSONable.show_json = show_json
+MSONable._ipython_display_ = _ipython_display_
 
 MODULE_PATH = Path(__file__).parents[0]
 
