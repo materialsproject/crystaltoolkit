ea6a5a61980af4fe6f610f1665bb932a6275e9c3
Matthew Horton <mkhorton@users.noreply.github.com>
2022-11-30 13:23:28 -0800

    Set version via `importlib.metadata`, use of `pkg_resources` discouraged
    See https://setuptools.pypa.io/en/latest/pkg_resources.html

--- crystal_toolkit/__init__.py
+++ crystal_toolkit/__init__.py
@@ -1,9 +1,9 @@
 from __future__ import annotations
 
 from pathlib import Path
+from importlib.metadata import PackageNotFoundError, version
 
 from monty.json import MSONable
-from pkg_resources import DistributionNotFound, get_distribution
 
 from crystal_toolkit.msonable import (
     _ipython_display_,
@@ -30,7 +30,7 @@ MSONable._ipython_display_ = _ipython_display_
 MODULE_PATH = Path(__file__).parents[0]
 
 try:
-    __version__ = get_distribution("crystal_toolkit").version
-except DistributionNotFound:  # pragma: no cover
+    __version__ = version("crystal_toolkit")
+except PackageNotFoundError:  # pragma: no cover
     # package is not installed
     pass
