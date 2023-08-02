b189cff38e03ab7ad69e89832c9ef9673a89a29f
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-08-02 13:25:25 -0700

    fix heading hierarchy in apps/examples/diffraction.py

--- crystal_toolkit/apps/examples/diffraction.py
+++ crystal_toolkit/apps/examples/diffraction.py
@@ -4,11 +4,10 @@ import dash
 from pymatgen.core import Lattice, Structure
 
 import crystal_toolkit.components as ctc
-from crystal_toolkit.helpers.layouts import H1, H3, Container
+from crystal_toolkit.helpers.layouts import H1, H2, Container
 from crystal_toolkit.settings import SETTINGS
 
 app = dash.Dash(assets_folder=SETTINGS.ASSETS_PATH)
-
 
 structure = Structure(Lattice.cubic(4.2), ["Na", "K"], [[0, 0, 0], [0.5, 0.5, 0.5]])
 
@@ -18,7 +17,7 @@ xrd_component = ctc.XRayDiffractionComponent(initial_structure=structure)
 layout = Container(
     [
         H1("XRDComponent Example"),
-        H3("Generated from Structure object"),
+        H2("Generated from Structure object", style={"fontSize": "1rem"}),
         xrd_component.layout(),
     ]
 )
